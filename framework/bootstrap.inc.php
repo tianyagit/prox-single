5df86fd9efedf31be50cb3feed643304debab069
RenChao <donknap@gmail.com>
2016-10-21 10:05:25 +0800

    重新整理项目文件，统一删除掉，然后再增加

--- framework/bootstrap.inc.php
+++ framework/bootstrap.inc.php
@@ -122,13 +122,13 @@ if (empty($_W['setting']['upload'])) {
 }
 $_W['attachurl'] = $_W['attachurl_local'] = $_W['siteroot'] . $_W['config']['upload']['attachdir'] . '/';
 if (!empty($_W['setting']['remote']['type'])) {
-	if ($_W['setting']['remote']['type'] == 1) {
+	if ($_W['setting']['remote']['type'] == ATTACH_FTP) {
 		$_W['attachurl'] = $_W['attachurl_remote'] = $_W['setting']['remote']['ftp']['url'] . '/';
-	} elseif ($_W['setting']['remote']['type'] == 2) {
+	} elseif ($_W['setting']['remote']['type'] == ATTACH_OSS) {
 		$_W['attachurl'] = $_W['attachurl_remote'] = $_W['setting']['remote']['alioss']['url'].'/';
-	} elseif ($_W['setting']['remote']['type'] == 3) {
+	} elseif ($_W['setting']['remote']['type'] == ATTACH_QINIU) {
 		$_W['attachurl'] = $_W['attachurl_remote'] = $_W['setting']['remote']['qiniu']['url'].'/';
-	} elseif ($_W['setting']['remote']['type'] == 4) {
+	} elseif ($_W['setting']['remote']['type'] == ATTACH_COS) {
 		$_W['attachurl'] = $_W['attachurl_remote'] = $_W['setting']['remote']['cos']['url'].'/';
 	}
 }
