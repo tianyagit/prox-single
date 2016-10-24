df28ded4fcdda1ccaaef8e61083999323d007021
RenChao <donknap@qq.com>
2016-10-24 13:55:17 +0800

    增加Jplayer组件

--- web/resource/js/app/util.js
+++ web/resource/js/app/util.js
@@ -1,5 +1,169 @@
 (function(window) {
 var util = {};
+util.iconBrowser = function(callback){
+	var footer = '<button type="button" class="btn btn-default" data-dismiss="modal">取消</button>';
+	var modalobj = util.dialog('请选择图标',['./index.php?c=utility&a=icon&callback=selectIconComplete'],footer,{containerName:'icon-container'});
+	modalobj.modal({'keyboard': false});
+	modalobj.find('.modal-dialog').css({'width':'70%'});
+	modalobj.find('.modal-body').css({'height':'70%','overflow-y':'scroll'});
+	modalobj.modal('show');
+
+	window.selectIconComplete = function(ico){
+		if($.isFunction(callback)){
+			callback(ico);
+			modalobj.modal('hide');
+		}
+	};
+}; // end of icon dialog
+
+util.emojiBrowser = function(callback){
+	require(['emoji'], function(){
+		var footer = '<button type="button" class="btn btn-default" data-dismiss="modal">取消</button>';
+		var modalobj = util.dialog('请选择表情', window.util.templates['emoji-content.tpl'], footer, {containerName:'icon-container'});
+		modalobj.modal({'keyboard': false});
+		modalobj.find('.modal-dialog').css({'width':'70%'});
+		modalobj.find('.modal-body').css({'height':'70%','overflow-y':'scroll'});
+		modalobj.modal('show');
+
+		window.selectEmojiComplete = function(emoji){
+			if($.isFunction(callback)){
+				callback(emoji);
+				modalobj.modal('hide');
+			}
+		};
+	});
+}; // end of emoji dialog
+
+util.linkBrowser = function(callback){
+	var footer = '<button type="button" class="btn btn-default" data-dismiss="modal">取消</button>';
+	var modalobj = util.dialog('请选择链接',['./index.php?c=utility&a=link&callback=selectLinkComplete'],footer,{containerName:'link-container'});
+	modalobj.modal({'keyboard': false});
+	modalobj.find('.modal-body').css({'height':'300px','overflow-y':'auto' });
+	modalobj.modal('show');
+
+	window.selectLinkComplete = function(link){
+		if($.isFunction(callback)){
+			callback(link);
+			modalobj.modal('hide');
+		}
+	};
+}; // end of icon dialo
+util.pageBrowser = function(callback, page){
+	var footer = '';
+	var modalobj = util.dialog('',['./index.php?c=utility&a=link&do=page&callback=pageLinkComplete&page='+ page],footer,{containerName:'link-container'});
+	modalobj.modal({'keyboard': false});
+	modalobj.find('.modal-body').css({'height':'700px','overflow-y':'auto' });
+	modalobj.modal('show');
+
+	window.pageLinkComplete = function(link, page){
+		if($.isFunction(callback)){
+			callback(link, page);
+			if (page == '' || page == undefined) {
+				modalobj.modal('hide');
+			}
+		}
+	};
+};
+util.newsBrowser = function(callback, page){
+	var footer = '';
+	var modalobj = util.dialog('',['./index.php?c=utility&a=link&do=news&callback=newsLinkComplete&page='+ page],footer,{containerName:'link-container'});
+	modalobj.modal({'keyboard': false});
+	modalobj.find('.modal-body').css({'height':'700px','overflow-y':'auto' });
+	modalobj.modal('show');
+
+	window.newsLinkComplete = function(link, page){
+		if($.isFunction(callback)){
+			callback(link, page);
+			if (page == '' || page == undefined) {
+				modalobj.modal('hide');
+			}
+		}
+	};
+};
+util.articleBrowser = function(callback, page){
+	var footer = '';
+	var modalobj = util.dialog('',['./index.php?c=utility&a=link&do=article&callback=articleLinkComplete&page='+ page],footer,{containerName:'link-container'});
+	modalobj.modal({'keyboard': false});
+	modalobj.find('.modal-body').css({'height':'700px','overflow-y':'auto' });
+	modalobj.modal('show');
+
+	window.articleLinkComplete = function(link, page){
+		if($.isFunction(callback)){
+			callback(link, page);
+			if (page == '' || page == undefined) {
+				modalobj.modal('hide');
+			}
+		}
+	};
+};
+
+util.phoneBrowser = function(callback, page){
+	var footer = '';
+	var modalobj = util.dialog('一键拨号',['./index.php?c=utility&a=link&do=phone&callback=phoneLinkComplete&page='+ page],footer,{containerName:'link-container'});
+	modalobj.modal({'keyboard': false});
+	modalobj.find('.modal-body').css({'height':'120px','overflow-y':'auto' });
+	modalobj.modal('show');
+
+	window.phoneLinkComplete = function(link, page){
+		if($.isFunction(callback)){
+			callback(link, page);
+			if (page == '' || page == undefined) {
+				modalobj.modal('hide');
+			}
+		}
+	};
+};
+
+util.showModuleLink = function(callback){
+	var footer = '';
+	var modalobj = util.dialog('模块链接选择',['./index.php?c=utility&a=link&do=modulelink&callback=moduleLinkComplete'], '');
+	modalobj.modal({'keyboard': false});
+	modalobj.find('.modal-body').css({'height':'700px','overflow-y':'auto' });
+	modalobj.modal('show');
+
+	window.moduleLinkComplete = function(link, permission){
+		if($.isFunction(callback)){
+			callback(link, permission);
+			modalobj.modal('hide');
+		}
+	};
+
+};
+util.colorpicker = function(elm, callback) {
+	require(['colorpicker'], function(){
+		$(elm).spectrum({
+			className : "colorpicker",
+			showInput: true,
+			showInitial: true,
+			showPalette: true,
+			maxPaletteSize: 10,
+			preferredFormat: "hex",
+			change: function(color) {
+				if($.isFunction(callback)) {
+					callback(color);
+				}
+			},
+			palette: [
+				["rgb(0, 0, 0)", "rgb(67, 67, 67)", "rgb(102, 102, 102)", "rgb(153, 153, 153)","rgb(183, 183, 183)",
+				"rgb(204, 204, 204)", "rgb(217, 217, 217)","rgb(239, 239, 239)", "rgb(243, 243, 243)", "rgb(255, 255, 255)"],
+				["rgb(152, 0, 0)", "rgb(255, 0, 0)", "rgb(255, 153, 0)", "rgb(255, 255, 0)", "rgb(0, 255, 0)",
+				"rgb(0, 255, 255)", "rgb(74, 134, 232)", "rgb(0, 0, 255)", "rgb(153, 0, 255)", "rgb(255, 0, 255)"],
+				["rgb(230, 184, 175)", "rgb(244, 204, 204)", "rgb(252, 229, 205)", "rgb(255, 242, 204)", "rgb(217, 234, 211)",
+				"rgb(208, 224, 227)", "rgb(201, 218, 248)", "rgb(207, 226, 243)", "rgb(217, 210, 233)", "rgb(234, 209, 220)",
+				"rgb(221, 126, 107)", "rgb(234, 153, 153)", "rgb(249, 203, 156)", "rgb(255, 229, 153)", "rgb(182, 215, 168)",
+				"rgb(162, 196, 201)", "rgb(164, 194, 244)", "rgb(159, 197, 232)", "rgb(180, 167, 214)", "rgb(213, 166, 189)",
+				"rgb(204, 65, 37)", "rgb(224, 102, 102)", "rgb(246, 178, 107)", "rgb(255, 217, 102)", "rgb(147, 196, 125)",
+				"rgb(118, 165, 175)", "rgb(109, 158, 235)", "rgb(111, 168, 220)", "rgb(142, 124, 195)", "rgb(194, 123, 160)",
+				"rgb(166, 28, 0)", "rgb(204, 0, 0)", "rgb(230, 145, 56)", "rgb(241, 194, 50)", "rgb(106, 168, 79)",
+				"rgb(69, 129, 142)", "rgb(60, 120, 216)", "rgb(61, 133, 198)", "rgb(103, 78, 167)", "rgb(166, 77, 121)",
+				"rgb(133, 32, 12)", "rgb(153, 0, 0)", "rgb(180, 95, 6)", "rgb(191, 144, 0)", "rgb(56, 118, 29)",
+				"rgb(19, 79, 92)", "rgb(17, 85, 204)", "rgb(11, 83, 148)", "rgb(53, 28, 117)", "rgb(116, 27, 71)",
+				"rgb(91, 15, 0)", "rgb(102, 0, 0)", "rgb(120, 63, 4)", "rgb(127, 96, 0)", "rgb(39, 78, 19)",
+				"rgb(12, 52, 61)", "rgb(28, 69, 135)", "rgb(7, 55, 99)", "rgb(32, 18, 77)", "rgb(76, 17, 48)"]
+			]
+		});
+	});
+}
 util.tomedia = function(src, forcelocal){
 	if(src.indexOf('http://') == 0 || src.indexOf('https://') == 0 || src.indexOf('./resource') == 0) {
 		return src;
@@ -636,170 +800,6 @@ util.encrypt = function (str) {
 		return str;
 	}
 };
-util.iconBrowser = function(callback){
-	var footer = '<button type="button" class="btn btn-default" data-dismiss="modal">取消</button>';
-	var modalobj = util.dialog('请选择图标',['./index.php?c=utility&a=icon&callback=selectIconComplete'],footer,{containerName:'icon-container'});
-	modalobj.modal({'keyboard': false});
-	modalobj.find('.modal-dialog').css({'width':'70%'});
-	modalobj.find('.modal-body').css({'height':'70%','overflow-y':'scroll'});
-	modalobj.modal('show');
-
-	window.selectIconComplete = function(ico){
-		if($.isFunction(callback)){
-			callback(ico);
-			modalobj.modal('hide');
-		}
-	};
-}; // end of icon dialog
-
-util.emojiBrowser = function(callback){
-	require(['emoji'], function(){
-		var footer = '<button type="button" class="btn btn-default" data-dismiss="modal">取消</button>';
-		var modalobj = util.dialog('请选择表情', window.util.templates['emoji-content.tpl'], footer, {containerName:'icon-container'});
-		modalobj.modal({'keyboard': false});
-		modalobj.find('.modal-dialog').css({'width':'70%'});
-		modalobj.find('.modal-body').css({'height':'70%','overflow-y':'scroll'});
-		modalobj.modal('show');
-
-		window.selectEmojiComplete = function(emoji){
-			if($.isFunction(callback)){
-				callback(emoji);
-				modalobj.modal('hide');
-			}
-		};
-	});
-}; // end of emoji dialog
-
-util.linkBrowser = function(callback){
-	var footer = '<button type="button" class="btn btn-default" data-dismiss="modal">取消</button>';
-	var modalobj = util.dialog('请选择链接',['./index.php?c=utility&a=link&callback=selectLinkComplete'],footer,{containerName:'link-container'});
-	modalobj.modal({'keyboard': false});
-	modalobj.find('.modal-body').css({'height':'300px','overflow-y':'auto' });
-	modalobj.modal('show');
-
-	window.selectLinkComplete = function(link){
-		if($.isFunction(callback)){
-			callback(link);
-			modalobj.modal('hide');
-		}
-	};
-}; // end of icon dialo
-util.pageBrowser = function(callback, page){
-	var footer = '';
-	var modalobj = util.dialog('',['./index.php?c=utility&a=link&do=page&callback=pageLinkComplete&page='+ page],footer,{containerName:'link-container'});
-	modalobj.modal({'keyboard': false});
-	modalobj.find('.modal-body').css({'height':'700px','overflow-y':'auto' });
-	modalobj.modal('show');
-
-	window.pageLinkComplete = function(link, page){
-		if($.isFunction(callback)){
-			callback(link, page);
-			if (page == '' || page == undefined) {
-				modalobj.modal('hide');
-			}
-		}
-	};
-};
-util.newsBrowser = function(callback, page){
-	var footer = '';
-	var modalobj = util.dialog('',['./index.php?c=utility&a=link&do=news&callback=newsLinkComplete&page='+ page],footer,{containerName:'link-container'});
-	modalobj.modal({'keyboard': false});
-	modalobj.find('.modal-body').css({'height':'700px','overflow-y':'auto' });
-	modalobj.modal('show');
-
-	window.newsLinkComplete = function(link, page){
-		if($.isFunction(callback)){
-			callback(link, page);
-			if (page == '' || page == undefined) {
-				modalobj.modal('hide');
-			}
-		}
-	};
-};
-util.articleBrowser = function(callback, page){
-	var footer = '';
-	var modalobj = util.dialog('',['./index.php?c=utility&a=link&do=article&callback=articleLinkComplete&page='+ page],footer,{containerName:'link-container'});
-	modalobj.modal({'keyboard': false});
-	modalobj.find('.modal-body').css({'height':'700px','overflow-y':'auto' });
-	modalobj.modal('show');
-
-	window.articleLinkComplete = function(link, page){
-		if($.isFunction(callback)){
-			callback(link, page);
-			if (page == '' || page == undefined) {
-				modalobj.modal('hide');
-			}
-		}
-	};
-};
-
-util.phoneBrowser = function(callback, page){
-	var footer = '';
-	var modalobj = util.dialog('一键拨号',['./index.php?c=utility&a=link&do=phone&callback=phoneLinkComplete&page='+ page],footer,{containerName:'link-container'});
-	modalobj.modal({'keyboard': false});
-	modalobj.find('.modal-body').css({'height':'120px','overflow-y':'auto' });
-	modalobj.modal('show');
-
-	window.phoneLinkComplete = function(link, page){
-		if($.isFunction(callback)){
-			callback(link, page);
-			if (page == '' || page == undefined) {
-				modalobj.modal('hide');
-			}
-		}
-	};
-};
-
-util.showModuleLink = function(callback){
-	var footer = '';
-	var modalobj = util.dialog('模块链接选择',['./index.php?c=utility&a=link&do=modulelink&callback=moduleLinkComplete'], '');
-	modalobj.modal({'keyboard': false});
-	modalobj.find('.modal-body').css({'height':'700px','overflow-y':'auto' });
-	modalobj.modal('show');
-
-	window.moduleLinkComplete = function(link, permission){
-		if($.isFunction(callback)){
-			callback(link, permission);
-			modalobj.modal('hide');
-		}
-	};
-
-};
-util.colorpicker = function(elm, callback) {
-	require(['colorpicker'], function(){
-		$(elm).spectrum({
-			className : "colorpicker",
-			showInput: true,
-			showInitial: true,
-			showPalette: true,
-			maxPaletteSize: 10,
-			preferredFormat: "hex",
-			change: function(color) {
-				if($.isFunction(callback)) {
-					callback(color);
-				}
-			},
-			palette: [
-				["rgb(0, 0, 0)", "rgb(67, 67, 67)", "rgb(102, 102, 102)", "rgb(153, 153, 153)","rgb(183, 183, 183)",
-				"rgb(204, 204, 204)", "rgb(217, 217, 217)","rgb(239, 239, 239)", "rgb(243, 243, 243)", "rgb(255, 255, 255)"],
-				["rgb(152, 0, 0)", "rgb(255, 0, 0)", "rgb(255, 153, 0)", "rgb(255, 255, 0)", "rgb(0, 255, 0)",
-				"rgb(0, 255, 255)", "rgb(74, 134, 232)", "rgb(0, 0, 255)", "rgb(153, 0, 255)", "rgb(255, 0, 255)"],
-				["rgb(230, 184, 175)", "rgb(244, 204, 204)", "rgb(252, 229, 205)", "rgb(255, 242, 204)", "rgb(217, 234, 211)",
-				"rgb(208, 224, 227)", "rgb(201, 218, 248)", "rgb(207, 226, 243)", "rgb(217, 210, 233)", "rgb(234, 209, 220)",
-				"rgb(221, 126, 107)", "rgb(234, 153, 153)", "rgb(249, 203, 156)", "rgb(255, 229, 153)", "rgb(182, 215, 168)",
-				"rgb(162, 196, 201)", "rgb(164, 194, 244)", "rgb(159, 197, 232)", "rgb(180, 167, 214)", "rgb(213, 166, 189)",
-				"rgb(204, 65, 37)", "rgb(224, 102, 102)", "rgb(246, 178, 107)", "rgb(255, 217, 102)", "rgb(147, 196, 125)",
-				"rgb(118, 165, 175)", "rgb(109, 158, 235)", "rgb(111, 168, 220)", "rgb(142, 124, 195)", "rgb(194, 123, 160)",
-				"rgb(166, 28, 0)", "rgb(204, 0, 0)", "rgb(230, 145, 56)", "rgb(241, 194, 50)", "rgb(106, 168, 79)",
-				"rgb(69, 129, 142)", "rgb(60, 120, 216)", "rgb(61, 133, 198)", "rgb(103, 78, 167)", "rgb(166, 77, 121)",
-				"rgb(133, 32, 12)", "rgb(153, 0, 0)", "rgb(180, 95, 6)", "rgb(191, 144, 0)", "rgb(56, 118, 29)",
-				"rgb(19, 79, 92)", "rgb(17, 85, 204)", "rgb(11, 83, 148)", "rgb(53, 28, 117)", "rgb(116, 27, 71)",
-				"rgb(91, 15, 0)", "rgb(102, 0, 0)", "rgb(120, 63, 4)", "rgb(127, 96, 0)", "rgb(39, 78, 19)",
-				"rgb(12, 52, 61)", "rgb(28, 69, 135)", "rgb(7, 55, 99)", "rgb(32, 18, 77)", "rgb(76, 17, 48)"]
-			]
-		});
-	});
-}
 if (typeof define === "function" && define.amd) {
 	define(function(){
 		return util;
