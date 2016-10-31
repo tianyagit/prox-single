1a600d9b3dfe62278008f359da3249d6d5c96712
yanwei <18735386269@163.com>
2016-10-31 17:54:41 +0800

    系统管理--常用系统工具--数据库相关操作

--- web/source/system/site.ctrl.php
+++ web/source/system/site.ctrl.php
@@ -4,14 +4,17 @@
  * [WeEngine System] Copyright (c) 2013 WE7.CC
  */
 defined('IN_IA') or exit('Access Denied');
+
 $dos = array('copyright');
 $do = in_array($do, $dos) ? $do : 'copyright';
+
 $settings = $_W['setting']['copyright'];
 if(empty($settings) || !is_array($settings)) {
 	$settings = array();
 } else {
 	$settings['slides'] = iunserializer($settings['slides']);
 }
+
 if ($do == 'copyright') {
 	$_W['page']['title'] = '站点信息设置 - 系统管理';
 	if (checksubmit('submit')) {
