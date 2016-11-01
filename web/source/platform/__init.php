b9ba588b34453bea4a3d05cace0457c0d7e0583b
RenChao <donknap@gmail.com>
2016-11-01 16:20:05 +0800

    整理代码

--- web/source/platform/__init.php
+++ web/source/platform/__init.php
@@ -1,6 +1,6 @@
 <?php
 /**
+ * 
  * [WeEngine System] Copyright (c) 2013 WE7.CC
- * $sn: pro/web/source/platform/__init.php : v 9f5368ee3726 : 2015/09/06 09:02:15 : yanghf $
  */
 define('FRAME', 'account');
