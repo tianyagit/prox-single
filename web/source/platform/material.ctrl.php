cddc320198b9a464505478f30523336f897de12e
cj <1262003227@qq.com>
2016-11-02 18:46:49 +0800

    新增图片，语音，视频素材列表页

--- web/source/platform/material.ctrl.php
+++ web/source/platform/material.ctrl.php
@@ -98,40 +98,62 @@ if ($do == 'sync') {
 
 if($do == 'list') {
 	$type = trim($_GPC['type']) ? trim($_GPC['type']) : 'news';
-	$condition = " as a RIGHT JOIN ". tablename('wechat_news')." as b ON a.id = b.attach_id WHERE a.uniacid = :uniacid AND a.type = :type AND a.model = :model AND a.media_id != ''";
-	$params = array(':uniacid' => $_W['uniacid'], ':type' => $type, ':model' => 'perm');
-	$id = intval($_GPC['id']);
-	$title = addslashes($_GPC['title']);
-	if(!empty($title)) {
-		$condition .= ' AND (b.title LIKE :title OR b.author LIKE :title OR b.digest LIKE :title)';
-		$params[':title'] = '%'.$title. "%";
-	}
-	$pageindex = max(1, intval($_GPC['page']));
-	$pagesize = 21;
-	$limit = " ORDER BY a.id DESC, b.id ASC LIMIT " . ($pageindex - 1) * $pagesize . ", {$pagesize}";
-	$total = pdo_fetchall("SELECT a.* FROM " . tablename('wechat_attachment') .$condition, $params, 'id');
-	$total = count($total);
-	$material_list = pdo_fetchall("SELECT a.* FROM " . tablename('wechat_attachment') .$condition . $limit, $params, 'id');
-	if(!empty($material_list)) {
-		foreach($material_list as &$material) {
-			if($type == 'video') {
-				$material['tag'] = iunserializer($row['tag']);
-			} elseif($type == 'news') {
-				$material['items'] = pdo_fetchall("SELECT * FROM " . tablename('wechat_news') . " WHERE uniacid = :uniacid AND attach_id = :attach_id ORDER BY displayorder ASC", array(':uniacid' => $_W['uniacid'], ':attach_id' => $material['id']));
-				if (!empty($material['items'])) {
-					$material['prompt_msg'] = false;
-					foreach($material['items'] as $material_row) {
-						if (empty($material_row['title']) || empty($material_row['thumb_url']) || empty($material_row['content'])) {
-							$material['prompt_msg'] = true;
-							break;
+	if ($type == 'news') {
+		$condition = " as a RIGHT JOIN " . tablename('wechat_news') . " as b ON a.id = b.attach_id WHERE a.uniacid = :uniacid AND a.type = :type AND a.model = :model AND a.media_id != ''";
+		$params = array(':uniacid' => $_W['uniacid'], ':type' => $type, ':model' => 'perm');
+		$id = intval($_GPC['id']);
+		$title = addslashes($_GPC['title']);
+		if (!empty($title)) {
+			$condition .= ' AND (b.title LIKE :title OR b.author LIKE :title OR b.digest LIKE :title)';
+			$params[':title'] = '%' . $title . "%";
+		}
+		$pageindex = max(1, intval($_GPC['page']));
+		$pagesize = 21;
+		$limit = " ORDER BY a.id DESC, b.id ASC LIMIT " . ($pageindex - 1) * $pagesize . ", {$pagesize}";
+		$total = pdo_fetchall("SELECT a.* FROM " . tablename('wechat_attachment') . $condition, $params, 'id');
+		$total = count($total);
+		$material_list = pdo_fetchall("SELECT a.* FROM " . tablename('wechat_attachment') . $condition . $limit, $params, 'id');
+		if (!empty($material_list)) {
+			foreach ($material_list as &$material) {
+				if ($type == 'video') {
+					$material['tag'] = iunserializer($row['tag']);
+				} elseif ($type == 'news') {
+					$material['items'] = pdo_fetchall("SELECT * FROM " . tablename('wechat_news') . " WHERE uniacid = :uniacid AND attach_id = :attach_id ORDER BY displayorder ASC", array(':uniacid' => $_W['uniacid'], ':attach_id' => $material['id']));
+					if (!empty($material['items'])) {
+						$material['prompt_msg'] = false;
+						foreach ($material['items'] as $material_row) {
+							if (empty($material_row['title']) || empty($material_row['thumb_url']) || empty($material_row['content'])) {
+								$material['prompt_msg'] = true;
+								break;
+							}
 						}
 					}
 				}
 			}
 		}
+		$pager = pagination($total, $pageindex, $pagesize);
 	}
-//	print_r($material_list);die;
-	$pager = pagination($total, $pageindex, $pagesize);
+	if ($type == 'image') {
+		$pageindex = max(1, intval($_GPC['page']));
+		$pagesize = 24;
+		$image_list = pdo_getslice('wechat_attachment', array('uniacid' => $_W['uniacid'], 'type' => 'image', 'model' => 'perm'), array($pageindex, $pagesize), $total, array(),'', 'id DESC');
+		$pager = pagination($total, $pageindex, $pagesize);
+	}
+	if ($type == 'voice') {
+		$pageindex = max(1, intval($_GPC['page']));
+		$pagesize = 24;
+		$voice_list = pdo_getslice('wechat_attachment', array('uniacid' => $_W['uniacid'], 'type' => 'voice', 'model' => 'perm'), array($pageindex, $pagesize), $total, array(),'', 'id DESC');
+		$pager = pagination($total, $pageindex, $pagesize);
+	}
+	if ($type == 'video') {
+		$pageindex = max(1, intval($_GPC['page']));
+		$pagesize = 24;
+		$video_list = pdo_getslice('wechat_attachment', array('uniacid' => $_W['uniacid'], 'type' => 'video', 'model' => 'perm'), array($pageindex, $pagesize), $total, array(),'', 'id DESC');
+		foreach($video_list as &$row) {
+			$row['tag'] = iunserializer($row['tag']);
+		}
+		$pager = pagination($total, $pageindex, $pagesize);
+	}
 }
 
 if($do == 'preview') {
