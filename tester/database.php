2fcfc9dfb4d940854e13618f88661935da316a82
yanwei <18735386269@163.com>
2016-11-09 14:55:16 +0800

    数据库测试用例

--- tester/database.php
+++ tester/database.php
@@ -3,26 +3,28 @@ use Testify\Testify;
 
 require '../framework/bootstrap.inc.php';
 require IA_ROOT . '/framework/library/testify/Testify.php';
-
+//防止30秒运行超时的错误（Maximum execution time of 30 seconds exceeded).
+set_time_limit(0);
 load()->func('communication');
 
 $tester = new Testify('测试数据库');
 
-$tester->test('数据库备份', function(){
+$tester->test('数据库备份', function() {
 	global $tester;
+	//登录
 	$loginurl = 'http://pros.we7.cc/web/index.php?c=user&a=login&';
 	$response = ihttp_get($loginurl);
 	preg_match('/name="token" value="(.*?)" type="hidden"/i', $response['content'], $matchs);
 	$token = $matchs['1'];
-		$username = 'admin';
-		$password = '123456';
-		$submit = '登录';
-		$post = array(
-				'username' => $username,
-				'password' => $password,
-				'submit' => $submit,
-				'token' => $token
-				);
+	$username = 'admin';
+	$password = '123456';
+	$submit = '登录';
+	$post = array(
+		'username' => $username,
+		'password' => $password,
+		'submit' => $submit,
+		'token' => $token
+	);
 	$responses = ihttp_request($loginurl, $post);
 	$login_success = '欢迎回来，admin。';
 	$result = strpos($responses['content'],$login_success);
@@ -33,7 +35,7 @@ $tester->test('数据库备份', function(){
 	}
 	$cookiejar =$responses['headers']['Set-Cookie'];
 	$tester->assertEquals($sign, true);
-	
+	//备份
 	$backupurl = 'http://pros.we7.cc/web/index.php?c=system&a=database&do=backup&status=1&start=2';
 	$response1 = ihttp_request($backupurl,'',array(
 		'CURLOPT_COOKIE' => implode(';' , $cookiejar)));
@@ -58,15 +60,15 @@ $tester->test('数据库备份', function(){
 				if (is_dir($dirname."/".$file)) {
 					$result = rmdi_r($dirname."/".$file);
 					$data = array(
-							'dirname'=> $result['dirname'],
-							'file'=> $result['file']
-							);
+						'dirname'=> $result['dirname'],
+						'file'=> $result['file']
+					);
 				}
 				if (is_file($dirname."/".$file)) {
 					$data = array(
-							'dirname'=> $dirname,
-							'file'=> $file
-							);
+						'dirname'=> $dirname,
+						'file'=> $file
+					);
 				}
 			}
 		//遍历完毕关闭文件
@@ -99,4 +101,91 @@ $tester->test('数据库备份', function(){
 	$tester->assertEquals($backup_sign, true);
 	
 });
-	$tester->run();
+
+$tester->test('数据库还原', function() {
+	global $tester ,$restore_volume_prefix ,$series , $dirname , $dir;
+	//登录
+	$loginurl = 'http://pros.we7.cc/web/index.php?c=user&a=login&';
+	$response = ihttp_get($loginurl);
+	preg_match('/name="token" value="(.*?)" type="hidden"/i', $response['content'], $matchs);
+	$token = $matchs['1'];
+	$username = 'admin';
+	$password = '123456';
+	$submit = '登录';
+	$post = array(
+		'username' => $username,
+		'password' => $password,
+		'submit' => $submit,
+		'token' => $token
+	);
+	$responses = ihttp_request($loginurl, $post);
+	$login_success = '欢迎回来，admin。';
+	$result = strpos($responses['content'],$login_success);
+	if (empty($result)) {
+		$sign = false;
+	} else {
+		$sign = true;
+	}
+	$cookiejar =$responses['headers']['Set-Cookie'];
+	$tester->assertEquals($sign, true);
+	//还原前抽样获取数据库某张表中有多少条记录
+	$start_old_sql = 'SELECT COUNT(*) FROM' .tablename(account);
+	$start_old_record_num = pdo_fetchcolumn($start_old_sql);
+	
+	$middle_old_sql = 'SELECT COUNT(*) FROM' .tablename(mc_mapping_fans1);
+	$middle_old_record_num = pdo_fetchcolumn($middle_old_sql);
+	
+	$end_old_sql = 'SELECT COUNT(*) FROM' .tablename(wechat_attachment);
+	$end_old_record_num = pdo_fetchcolumn($end_old_sql);
+	//还原备份的目录
+	$dir = IA_ROOT . '/data/backup/';
+	$dirname = "1478487306_e36334Nr";
+	$restore_dirname = $dir . $dirname;
+	//统计备份文件的数量
+	$num = count(scandir($restore_dirname));
+	//去掉本目录和上级目录的点
+	$restore_file_num = $num -2;
+	//开始还原备份
+	for ( $i=1; $i<=$restore_file_num; $i++) {
+		if ($i==1) {
+			$restoreurl = "http://pros.we7.cc/web/index.php?c=system&a=database&do=restore&restore_dirname=" .$dirname;
+		} else {
+			$restoreurl = "http://pros.we7.cc/web/index.php?c=system&a=database&do=restore&restore_dirname=" .$dirname ."&restore_volume_prefix=" . $restore_volume_prefix . "&restore_volume_sizes=" . $series;
+		}
+		$response1 = ihttp_request($restoreurl,'',array(
+			'CURLOPT_COOKIE' => implode(';' , $cookiejar)));
+		preg_match('/href="\.\/index.php\?c=system&a=database&do=restore&restore_dirname=(.*?)&restore_volume_prefix=(.*?)&restore_volume_sizes=(.*?)"/i', $response1['content'], $match);
+	 	$restore_volume_prefix = $match[2];
+		$series = $match[3]-1;
+	 	$restore_file = $dir . $match[1] . "/volume-" . $restore_volume_prefix . "-" . $series . ".sql";
+		//备份目录下是否有该备份文件
+		if (is_file($restore_file)) {
+			$restore_sign . "_" . $series = true;
+		} else {
+			$restore_sign . "_" . $series = false;
+		}
+		$tester->assertEquals($restore_sign . "_" . $series, true);
+		//是否提醒成功恢复该备份文件
+		$restore_success = "正在恢复数据备份, 请不要关闭浏览器, 当前第" . $series ."卷";
+		$result1 = strpos($response1['content'],$restore_success);
+		if (empty($result1)) {
+			$restore_success_sign . "_" . $series = false;
+		} else {
+			$restore_success_sign . "_" . $series = true;
+		}
+		$tester->assertEquals($restore_success_sign . "_" . $series, true);
+	}
+	//某张表在还原后与还原前对比
+	$start_new_sql = 'SELECT COUNT(*) FROM' .tablename(account);
+	$start_new_record_num = pdo_fetchcolumn($start_new_sql);
+	$tester->assertEquals($start_old_record_num, $start_new_record_num);
+	
+	$middle_new_sql = 'SELECT COUNT(*) FROM' .tablename(mc_mapping_fans1);
+	$middle_new_record_num = pdo_fetchcolumn($middle_new_sql);
+	$tester->assertEquals($middle_old_record_num, $middle_new_record_num);
+	
+	$end_new_sql = 'SELECT COUNT(*) FROM' .tablename(wechat_attachment);
+	$end_new_record_num = pdo_fetchcolumn($end_new_sql);
+	$tester->assertEquals($end_old_record_num, $end_new_record_num);
+});
+$tester->run();
