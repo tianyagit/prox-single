62adaccf159676d53e02aab8de7fa23c9fc802d4
RenChao <donknap@gmail.com>
2016-11-17 15:32:43 +0800

    模块首页导航和个人中心导航

--- web/source/site/nav.ctrl.php
+++ web/source/site/nav.ctrl.php
@@ -5,12 +5,38 @@
  */
 defined('IN_IA') or exit('Access Denied');
 
-$dos = array('home', 'uc');
+load()->model('module');
+
+$dos = array('home', 'profile');
 $do = !empty($_GPC['do']) ? $_GPC['do'] : 'home';
 
 uni_user_permission_check('platform_nav_' . $do, true, 'nav');
 $modulename = $_GPC['m'];
 
-if ($do == 'home' || $do == 'uc') {
+//首页导航
+if ($do == 'home' || $do == 'profile') {
+	$modules = uni_modules();
+	$bindings = array();
 	
+	if(!empty($modulename)) {
+		$modulenames = array($modulename);
+	} else {
+		$modulenames = array_keys($modules);
+	}
+	
+	foreach($modulenames as $modulename) {
+		$entries = module_entries($modulename, array($do));
+		if(!empty($entries[$do])) {
+			$bindings[$modulename] = $entries[$do];
+		}
+	}
+	$entries = array();
+	if(!empty($bindings)) {
+		foreach($bindings as $modulename => $group) {
+			foreach($group as $bind) {
+				$entries[] = array('module' => $modulename, 'from' => $bind['from'], 'title' => $bind['title'], 'url' => $bind['url']);
+			}
+		}
+	}
+	template('site/nav');
 }
