2804dfa28936d7a969050ac8681f4c68e4949673
cj <1262003227@qq.com>
2016-11-19 17:11:01 +0800

    邮箱配置参数代码修改

--- web/source/profile/notify.ctrl.php
+++ web/source/profile/notify.ctrl.php
@@ -23,14 +23,18 @@ if ($do == 'mail') {
 			'signature' => $_GPC['signature'],
 		);
 		$setting = array('notify' => iserializer($notify));
+		$original_setting = uni_setting_load('notify');
 		pdo_update('uni_settings', $setting, array('uniacid' => $_W['uniacid']));
 		load()->func('communication');
 		$result = ihttp_email($notify['mail']['username'], $_W['account']['name'] . '验证邮件'.date('Y-m-d H:i:s'), '如果您收到这封邮件则表示您系统的发送邮件配置成功！');
 		if (is_error($result)) {
-			message('配置失败，请检查配置信息');
+			$setting = array('notify' => iserializer($original_setting));
+			pdo_update('uni_settings', $setting, array('uniacid' => $_W['uniacid']));
+			message('配置失败，请检查配置信息', url('profile/notify'), 'info');
+		} else {
+			cache_delete("unisetting:{$_W['uniacid']}");
+			message('配置成功！', url('profile/notify',array('do' => 'mail')));
 		}
-		cache_delete("unisetting:{$_W['uniacid']}");
-		message('更新设置成功！', url('profile/notify',array('do' => 'mail')));
 	}
 	$notify_setting = uni_setting_load('notify');
 	$mail_setting = empty($notify_setting['notify']['mail'])? array() : $notify_setting['notify']['mail'];
