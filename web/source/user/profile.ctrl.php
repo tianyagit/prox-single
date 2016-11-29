8986be2eadfb0ab7d71631e5235b5b4a09662c4d
brjun <351409246@qq.com>
2016-11-29 17:08:43 +0800

    微擎新版：系统管理->我的账户

--- web/source/user/profile.ctrl.php
+++ web/source/user/profile.ctrl.php
@@ -4,24 +4,59 @@
  */
 defined('IN_IA') or exit('Access Denied');
 
-$dos = array('application','base','account');
+$dos = array('base', 'post');
 $do = in_array($do, $dos) ? $do : 'base';
-
-$uid = intval($_GPC['uid'])?intval($_GPC['uid']):$_W['uid'];
+load()->model('user');
+$uid = intval($_GPC['uid']) ? intval($_GPC['uid']) : $_W['uid'];
 $user = user_single($uid);
+
+if($do == 'post' && $_W['isajax'] && $_W['ispost']) {
+	$post = $_GPC['__input'];
+	$type = $post['type'];
+	if(empty($user)) message('-1', 'ajax', 'error');
+	switch ($type) {
+		case 'avatar':
+			$result = pdo_update('users_profile', array('avatar' => $post['avatar']), array('uid' => $_W['uid']));
+			break;
+		case 'username':
+			$result = pdo_update('users', array('username' => $post['username']), array('uid' => $_W['uid']));
+			break;
+		case 'password':
+			if($post['newpwd'] !== $post['renewpwd']) message('2', 'ajax', 'error');
+			$pwd = user_hash($post['oldpwd'], $user['salt']);
+			if($pwd != $user['password']) message('3', 'ajax', 'error');
+			$newpwd = user_hash($post['newpwd'], $user['salt']);
+			$result = pdo_update('users', array('password' => $newpwd), array('uid' => $_W['uid']));
+			break;
+		case 'realname':
+			$result = pdo_update('users_profile', array('realname' => $post['realname']), array('uid' => $_W['uid']));
+			break;
+		case 'birth':
+			$result = pdo_update('users_profile', array('birthyear' => $post['year'], 'birthmonth' => $post['month'], 'birthday' => $post['day']), array('uid' => $_W['uid']));
+			break;
+		case 'address':
+			$result = pdo_update('users_profile', array('address' => $post['address']), array('uid' => $_W['uid']));
+			break;
+		case 'reside':
+			$result = pdo_update('users_profile', array('resideprovince' => $post['province'], 'residecity' => $post['city'], 'residedist' => $post['district']), array('uid' => $_W['uid']));
+			break;
+	}
+	if($result) {
+		message('0', 'ajax', 'success');
+	}else {
+		message('1', 'ajax', 'error');
+	}
+}
+
 //账号信息
 if ($do == 'base') {
 	$_W['page']['title'] = '账号信息 - 我的账户 - 用户管理';
-	$sql = "SELECT username, password, salt, groupid, lastvisit, lastip , starttime, endtime FROM " . tablename('users') . " WHERE `uid` = '{$_W['uid']}'";
-	$user = pdo_fetch($sql);
 
 	if (empty($user)) {
 		message('抱歉，用户不存在或是已经被删除！', url('user/profile'), 'error');
 	}
-	$user['groupname'] = pdo_fetchcolumn('SELECT name FROM ' . tablename('users_group') . ' WHERE id = :id', array(':id' => $user['groupid']));
-	$user['role'] = pdo_fetchcolumn('SELECT role FROM ' . tablename('uni_account_users') . ' WHERE uid = :uid', array(':uid' => $_W['uid']));
+	$user['last_visit'] = date('Y-m-d H:i:s', $user['lastvisit']);
 	
-	$extendfields = pdo_fetchall("SELECT field, title, description, required FROM ".tablename('profile_fields')." WHERE available = '1' AND showinregister = '1' ORDER BY displayorder DESC");
 	$profile = pdo_fetch('SELECT * FROM '.tablename('users_profile').' WHERE `uid` = :uid LIMIT 1',array(':uid' => $_W['uid']));
 	$profile['reside'] = array(
 		'province' => $profile['resideprovince'],
@@ -33,33 +68,8 @@ if ($do == 'base') {
 		'month' => $profile['birthmonth'],
 		'day' => $profile['birthday'],
 	);
+	$profile['avatar'] = tomedia($profile['avatar']);
 	$profile['resides'] = $profile['resideprovince'] . $profile['residecity'] . $profile['residedist'] ;
 	$profile['births'] = $profile['birthyear'] . '年' . $profile['birthmonth'] . '月' . $profile['birthday'] .'日' ;
+	template('user/profile');
 }
-
-//应用
-if ($do == 'application') {
-}
-//公众号列表
-if ($do == 'account') {
-	//获取用户组信息
-	if (!empty($user['groupid'])) {
-		$group = pdo_fetch("SELECT * FROM ".tablename('users_group')." WHERE id = '{$user['groupid']}'");
-		if (!empty($group)) {
-			$package = iunserializer($group['package']);
-			$group['package'] = uni_groups($package);
-		}
-	}
-	$weids = pdo_fetchall("SELECT uniacid, role FROM ".tablename('uni_account_users')." WHERE uid = '$uid'", array(), 'uniacid');
-	if (!empty($weids)) {
-		$wechats = pdo_fetchall("SELECT * FROM ".tablename('uni_account')." WHERE uniacid IN (".implode(',', array_keys($weids)).")");
-		foreach ($wechats as $key => $wechat) {
-			$sql = "SELECT acid , level FROM " . tablename('account_wechats') . " WHERE `uniacid` = '{$wechat['uniacid']}'";
-			$account = pdo_fetch($sql);
-			$wechats[$key]['acid'] = $account['acid'];
-			$wechats[$key]['level'] = $account['level'];
-		}
-	}
-}
-
-template('user/profile');
