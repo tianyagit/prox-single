35b161c4c35a562365dc187393a9e94cfdf15f31
brjun <351409246@qq.com>
2016-12-06 17:10:44 +0800

    微擎新版：系统管理->用户管理（添加用户）

--- web/source/user/create.ctrl.php
+++ web/source/user/create.ctrl.php
@@ -4,26 +4,28 @@
  */
 defined('IN_IA') or exit('Access Denied');
 
-load()->model('user');
-
-$_W['page']['title'] = '添加用户 - 用户管理 - 账号/用户';
+$_W['page']['title'] = '添加用户 - 用户管理';
+uni_user_permission_check('system_user_display');
 
 if (checksubmit()) {
-	$user = array();
-	$user['username'] = trim($_GPC['username']);
-	if (!preg_match(REGULAR_USERNAME, $user['username'])) {
+	load()->model('user');
+	$username = trim($_GPC['username']);
+	if (!preg_match(REGULAR_USERNAME, $username)) {
 		message('必须输入用户名，格式为 3-15 位字符，可以包括汉字、字母（不区分大小写）、数字、下划线和句点。');
 	}
-	if (user_check(array('username' => $user['username']))) {
+	if (user_check(array('username' => $username))) {
 		message('非常抱歉，此用户名已经被注册，你需要更换注册名称！');
 	}
-	$user['password'] = $_GPC['password'];
-	if (istrlen($user['password']) < 8) {
+	if (istrlen($_GPC['password']) < 8) {
 		message('必须输入密码，且密码长度不得低于8位。');
 	}
-	$user['remark'] = $_GPC['remark'];
-	$user['groupid'] = intval($_GPC['groupid']) ? intval($_GPC['groupid']) : message('请选择所属用户组');
-	$group = pdo_fetch("SELECT id,timelimit FROM ".tablename('users_group')." WHERE id = :id", array(':id' => $user['groupid']));
+	if($_GPC['password'] !== $_GPC['repassword']) {
+		message('两次密码不一致！');
+	}
+	if(!intval($_GPC['groupid'])) {
+		message('请选择所属用户组');
+	}
+	$group = pdo_fetch("SELECT id,timelimit FROM ".tablename('users_group')." WHERE id = :id", array(':id' => intval($_GPC['groupid'])));
 	if (empty($group)) {
 		message('会员组不存在');
 	}
@@ -32,11 +34,17 @@ if (checksubmit()) {
 	if ($timelimit > 0) {
 		$timeadd = strtotime($timelimit . ' days');
 	}
-	$user['starttime'] = TIMESTAMP;
-	$user['endtime'] = $timeadd;
-	$uid = user_register($user);
+	$data = array(
+		'username' => $username,
+		'password' => $_GPC['password'],
+		'remark' => $_GPC['remark'],
+		'groupid' => intval($_GPC['groupid']),
+		'starttime' => TIMESTAMP,
+		'endtime' => $timeadd,
+	);
+	$uid = user_register($data);
 	if ($uid > 0) {
-		unset($user['password']);
+		unset($data);
 		message('用户增加成功！', url('user/edit', array('uid' => $uid)));
 	}
 	message('增加用户失败，请稍候重试或联系网站管理员解决！');
