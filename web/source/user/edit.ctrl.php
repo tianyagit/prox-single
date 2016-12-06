3b31bb0012beea14e6bd024b76d132f6d2e2d020
brjun <351409246@qq.com>
2016-12-06 19:06:25 +0800

    微擎新版：系统管理->用户管理（编辑用户基本信息）

--- web/source/user/edit.ctrl.php
+++ web/source/user/edit.ctrl.php
@@ -5,43 +5,53 @@
  */
 defined('IN_IA') or exit('Access Denied');
 
-load()->model('setting');
-load()->model('user');
 
-$_W['page']['title'] = '编辑用户 - 用户管理 - 账号/用户';
+$_W['page']['title'] = '编辑用户 - 用户管理';
+uni_user_permission_check('system_user_display');
 
 $do = $_GPC['do'];
-$dos = array('edit');
-$do = in_array($do, $dos) ? $do: 'edit';
+$dos = array('edit_base', 'edit_modules_tpl', 'edit_account');
+$do = in_array($do, $dos) ? $do: 'edit_base';
 
+// load()->model('setting');
+load()->model('user');
 $uid = intval($_GPC['uid']);
 $user = user_single($uid);
+if (empty($user)) {
+	message('访问错误, 未找到该操作员.', url('user/display'), 'error');
+}else {
+	if($user['status'] == 1) message('访问错误，该用户未审核通过，请先审核通过再修改！', url('user/display/check_display'), 'error');
+	if($user['status'] == 3) message('访问错误，该用户已被禁用，请先启用再修改！', url('user/display/recycle_display'), 'error');
+}
 $founders = explode(',', $_W['config']['setting']['founder']);
 
-//编辑用户
-if ($do == 'edit') {
-	if (empty($user)) {
-		message('访问错误, 未找到指定操作员.', url('user/display'), 'error');
+//编辑用户基础信息
+if ($do == 'edit_base') {
+	$user['last_visit'] = date('Y-m-d H:i:s', $user['lastvisit']);
+	$profile = pdo_fetch('SELECT * FROM '.tablename('users_profile').' WHERE `uid` = :uid LIMIT 1',array(':uid' => $uid));
+	if(!empty($profile)) {
+		$profile['reside'] = array(
+			'province' => $profile['resideprovince'],
+			'city' => $profile['residecity'],
+			'district' => $profile['residedist']
+		);
+		$profile['birth'] = array(
+			'year' => $profile['birthyear'],
+			'month' => $profile['birthmonth'],
+			'day' => $profile['birthday'],
+		);
+		$profile['avatar'] = tomedia($profile['avatar']);
+		$profile['resides'] = $profile['resideprovince'] . $profile['residecity'] . $profile['residedist'] ;
+		$profile['births'] = $profile['birthyear'] . '年' . $profile['birthmonth'] . '月' . $profile['birthday'] .'日' ;
 	}
-	$extendfields = pdo_fetchall("SELECT field, title, description, required FROM ".tablename('profile_fields')." WHERE available = '1' AND showinregister = '1'");
-	$user['profile'] = pdo_fetch("SELECT * FROM ".tablename('users_profile')." WHERE uid = :uid", array(':uid' => $uid));
-	$user['profile']['reside'] = array(
-		'province' => $user['profile']['resideprovince'],
-		'city' => $user['profile']['residecity'],
-		'district' => $user['profile']['residedist'],
-	);
-	unset($user['profile']['resideprovince']);
-	unset($user['profile']['residecity']);
-	unset($user['profile']['residedist']);
-	$user['profile']['birth'] = array(
-		'year' => $user['profile']['birthyear'],
-		'month' => $user['profile']['birthmonth'],
-		'day' => $user['profile']['birthday'],
-	);
-	unset($user['profile']['birthyear']);
-	unset($user['profile']['birthmonth']);
-	unset($user['profile']['birthday']);
-	$groups = pdo_fetchall("SELECT id, name FROM ".tablename('users_group')." ORDER BY id ASC");
-	template('user/edit');
+	template('user/edit-base');
+}
+if($do == 'edit_modules_tpl') {
+	echo 'modules_tpl';
+	template('user/edit-modules-tpl');
 }
 
+if($do == 'edit_account') {
+	echo 'account';
+	template('user/edit-account');
+}
