4032180f2eeac613af572edfc7050331ac94ab84
brjun <351409246@qq.com>
2016-12-15 14:34:48 +0800

    微擎新版：公众号管理（应用模块/模板、短信）

--- web/source/account/post.ctrl.php
+++ web/source/account/post.ctrl.php
@@ -6,6 +6,7 @@
 defined('IN_IA') or exit('Access Denied');
 
 load()->model('module');
+load()->model('cloud');
 
 $dos = array('base', 'sms', 'users', 'modules_tpl');
 $do = in_array($do, $dos) ? $do : 'base';
@@ -121,6 +122,50 @@ if($do == 'base') {
 }
 
 if($do == 'sms') {
+	$settings = uni_setting($uniacid, array('notify'));
+	$notify = $settings['notify'] ? $settings['notify'] : array();
+
+	$sms_info = cloud_sms_info();
+	$max_num = empty($sms_info['sms_count']) ? 0 : $sms_info['sms_count'];
+	$signatures = $sms_info['sms_sign'];
+
+	if ($_W['isajax'] && $_W['ispost'] && $_GPC['type'] == 'balance') {
+		if ($max_num == 0) {
+			message(error(-1), '', 'ajax');
+		}
+		$balance = intval($_GPC['balance']);
+		$notify['sms']['balance'] = $balance;
+		$notify['sms']['balance'] = min(max(0, $notify['sms']['balance']), $max_num);
+		$count_num = $max_num - $notify['sms']['balance'];
+		$num = $notify['sms']['balance'];
+		$notify = iserializer($notify);
+		$updatedata['notify'] = $notify;
+		$result = pdo_update('uni_settings', $updatedata , array('uniacid' => $uniacid));
+		if($result){
+			message(error(0, array('count' => $count_num, 'num' => $num)), '', 'ajax');
+		}else {
+			message(error(1), '', 'ajax');
+		}
+	}
+	if($_W['isajax'] && $_W['ispost'] && $_GPC['type'] == 'signature') {
+		if (!empty($_GPC['signature'])) {
+			$signature = trim($_GPC['signature']);
+			$setting = pdo_get('uni_settings', array('uniacid' => $uniacid));
+			$notify = iunserializer($setting['notify']);
+			$notify['sms']['signature'] = $signature;
+
+			$notify = serialize($notify);
+			$result = pdo_update('uni_settings', array('notify' => $notify), array('uniacid' => $uniacid));
+			if($result) {
+				message(error(0), '', 'ajax');
+			}else {
+				message(error(1), '', 'ajax');
+			}
+		}else {
+			message(error(40035), '', 'ajax');
+		}
+	}
+
 	template('account/manage-sms');
 }
 
@@ -129,5 +174,111 @@ if($do == 'users') {
 }
 
 if($do == 'modules_tpl') {
+	$unigroups = uni_groups();
+	$ownerid = pdo_fetchcolumn("SELECT uid FROM ".tablename('uni_account_users')." WHERE uniacid = :uniacid AND role = 'owner'", array(':uniacid' => $uniacid));
+	$owner = user_single(array('uid' => $ownerid));
+	if($_W['isajax'] && $_W['ispost']) {
+		if($_GPC['type'] == 'group') {
+			if(!empty(trim($_GPC['groupdata']))) {
+				$groups = explode(',', trim($_GPC['groupdata']));
+				//附加套餐组
+				pdo_delete('uni_account_group', array('uniacid' => $uniacid));
+				$group = pdo_get('users_group', array('id' => $owner['groupid']));
+				$group['package'] = iunserializer($group['package']);
+				$group['package'] = array_unique($group['package']);
+				foreach ($groups as $packageid) {
+					if (!empty($packageid) && !in_array($packageid, $group['package'])) {
+						pdo_insert('uni_account_group', array(
+							'uniacid' => $uniacid,
+							'groupid' => $packageid,
+						));
+					}
+				}
+				message(error(0), '', 'ajax');
+			}else {
+				message(error(40035), '', 'ajax');
+			}
+		}
+
+		if($_GPC['type'] == 'extend') {
+			//如果有附加的权限，则生成专属套餐组
+			
+			$module = trim($_GPC['module']);
+			$tpl = trim($_GPC['tpl']);
+			if (!empty($module) || !empty($tpl)) {
+				$module = explode(',', $module);
+				$tpl = explode(',', $tpl);
+				$data = array(
+					'modules' => iserializer($module),
+					'templates' => iserializer($tpl),
+					'uniacid' => $uniacid,
+					'name' => '',
+				);
+				$id = pdo_fetchcolumn("SELECT id FROM ".tablename('uni_group')." WHERE uniacid = :uniacid", array(':uniacid' => $uniacid));
+				if (empty($id)) {
+					pdo_insert('uni_group', $data);
+				} else {
+					pdo_update('uni_group', $data, array('id' => $id));
+				}
+			} else {
+				pdo_delete('uni_group', array('uniacid' => $uniacid));
+			}
+			message(error(0), '', 'ajax');
+		}
+		message(error(40035), '', 'ajax');
+	}
+	$modules_tpl = $extend = array();
+	if (!empty($ownerid)) {
+		$owner['group'] = pdo_fetch("SELECT id, name, package FROM ".tablename('users_group')." WHERE id = :id", array(':id' => $owner['groupid']));
+		$owner['group']['package'] = iunserializer($owner['group']['package']);
+		if(!empty($owner['group']['package'])){
+			foreach ($owner['group']['package'] as $package_value) {
+				if($package_value == -1){
+					$modules_tpl[] = array(
+							'id' => -1,
+							'name' => '所有服务',
+							'modules' => array(array('name' => 'all', 'title' => '所有模块')),
+							'templates' => array(array('name' => 'all', 'title' => '所有模板')),
+						);
+				}elseif ($package_value == 0) {
+					
+				}else {
+					$modules_tpl[] = $unigroups[$package_value];
+				}
+			}
+		}
+		//附加套餐
+		$extendpackage = pdo_getall('uni_account_group', array('uniacid' => $uniacid), array(), 'groupid');
+		if(!empty($extendpackage)) {
+			foreach ($extendpackage as $extendpackage_val) {
+				if($extendpackage_val['groupid'] == -1){
+					$modules_tpl[] = array(
+							'id' => -1,
+							'name' => '所有服务',
+							'modules' => array(array('name' => 'all', 'title' => '所有模块')),
+							'templates' => array(array('name' => 'all', 'title' => '所有模板')),
+						);
+				}elseif ($extendpackage_val['groupid'] == 0) {
+					
+				}else {
+					$modules_tpl[] = $unigroups[$extendpackage_val['groupid']];
+				}
+			}
+		}
+		//附加权限
+		$modules = pdo_fetchall("SELECT mid, name, title FROM " . tablename('modules') . ' WHERE issystem != 1', array(), 'name');
+		$templates  = pdo_fetchall("SELECT * FROM ".tablename('site_templates'));
+		$extend = pdo_fetch("SELECT * FROM ".tablename('uni_group')." WHERE uniacid = :uniacid", array(':uniacid' => $uniacid));
+		$extend['modules'] = iunserializer($extend['modules']);
+		$extend['templates'] = iunserializer($extend['templates']);
+		if (!empty($extend['modules'])) {
+			$extend['modules'] = pdo_getall('modules', array('name' => $extend['modules']), array('title', 'name'));
+		}
+		if (!empty($extend['templates'])) {
+			$extend['templates'] = pdo_getall('site_templates', array('id' => $extend['templates']), array('title', 'name'));
+		}
+	}else {
+		message('公众号错误！', referer(), 'error');
+	}
 	template('account/manage-modules-tpl');
 }
