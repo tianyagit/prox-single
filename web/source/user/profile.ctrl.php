da7d949dfc856867f7a40f411750f75321e1bb92
brjun <351409246@qq.com>
2016-12-19 17:46:47 +0800

    微擎新版：修复修改用户基本信息无数据时报错

--- web/source/user/profile.ctrl.php
+++ web/source/user/profile.ctrl.php
@@ -17,19 +17,47 @@ if($do == 'post' && $_W['isajax'] && $_W['ispost']) {
 
 	$uid = is_array($post['uid']) ? 0 : intval($post['uid']);
 	if(empty($uid)) message('-1', 'ajax', 'error');
+	$users_profile_exist = pdo_get('users_profile', array('uid' => $uid));
 	switch ($type) {
 		case 'avatar':
-			$result = pdo_update('users_profile', array('avatar' => $post['avatar']), array('uid' => $uid));
+			if($users_profile_exist) {
+				$result = pdo_update('users_profile', array('avatar' => $post['avatar']), array('uid' => $uid));
+			}else {
+				$data = array(
+						'uid' => $uid,
+						'createtime' => TIMESTAMP,
+						'avatar' => $post['avatar']
+					);
+				$result = pdo_insert('users_profile', $data);
+			}
 			break;
 		case 'username':
-			$result = pdo_update('users', array('username' => $post['username']), array('uid' => $uid));
+			if($users_profile_exist) {
+				$result = pdo_update('users', array('username' => $post['username']), array('uid' => $uid));
+			}else {
+				$data = array(
+						'uid' => $uid,
+						'createtime' => TIMESTAMP,
+						'username' => trim($post['username'])
+					);
+				$result = pdo_insert('users_profile', $data);
+			}
 			break;
 		case 'password':
 			if($post['newpwd'] !== $post['renewpwd']) message('2', 'ajax', 'error');
 			$pwd = user_hash($post['oldpwd'], $user['salt']);
 			if($pwd != $user['password']) message('3', 'ajax', 'error');
 			$newpwd = user_hash($post['newpwd'], $user['salt']);
-			$result = pdo_update('users', array('password' => $newpwd), array('uid' => $uid));
+			if($users_profile_exist) {
+				$result = pdo_update('users', array('password' => $newpwd), array('uid' => $uid));
+			}else {
+				$data = array(
+						'uid' => $uid,
+						'createtime' => TIMESTAMP,
+						'password' => $newpwd
+					);
+				$result = pdo_insert('users_profile', $data);
+			}
 			break;
 		case 'endtime' :
 			if($post['endtype'] == 1) {
@@ -37,19 +65,68 @@ if($do == 'post' && $_W['isajax'] && $_W['ispost']) {
 			}else {
 				$endtime = strtotime($post['endtime']);
 			}
-			$result = pdo_update('users', array('endtime' => $endtime), array('uid' => $uid));
+			if($users_profile_exist) {
+				$result = pdo_update('users', array('endtime' => $endtime), array('uid' => $uid));
+			}else {
+				$data = array(
+						'uid' => $uid,
+						'createtime' => TIMESTAMP,
+						'endtime' => $endtime
+					);
+				$result = pdo_insert('users_profile', $data);
+			}
 			break;
 		case 'realname':
-			$result = pdo_update('users_profile', array('realname' => $post['realname']), array('uid' => $uid));
+			if($users_profile_exist) {
+				$result = pdo_update('users_profile', array('realname' => $post['realname']), array('uid' => $uid));
+			}else {
+				$data = array(
+						'uid' => $uid,
+						'createtime' => TIMESTAMP,
+						'realname' => trim($post['realname'])
+					);
+				$result = pdo_insert('users_profile', $data);
+			}
 			break;
 		case 'birth':
-			$result = pdo_update('users_profile', array('birthyear' => $post['year'], 'birthmonth' => $post['month'], 'birthday' => $post['day']), array('uid' => $uid));
+			if($users_profile_exist) {
+				$result = pdo_update('users_profile', array('birthyear' => $post['year'], 'birthmonth' => $post['month'], 'birthday' => $post['day']), array('uid' => $uid));
+			}else {
+				$data = array(
+						'uid' => $uid,
+						'createtime' => TIMESTAMP,
+						'birthyear' => $post['year'],
+						'birthmonth' => $post['month'],
+						'birthday' => $post['day']
+					);
+				$result = pdo_insert('users_profile', $data);
+			}
 			break;
 		case 'address':
-			$result = pdo_update('users_profile', array('address' => $post['address']), array('uid' => $uid));
+			if($users_profile_exist) {
+				$result = pdo_update('users_profile', array('address' => $post['address']), array('uid' => $uid));
+			}else {
+				$data = array(
+						'uid' => $uid,
+						'createtime' => TIMESTAMP,
+						'address' => trim($post['address'])
+					);
+				$result = pdo_insert('users_profile', $data);
+			}
 			break;
 		case 'reside':
-			$result = pdo_update('users_profile', array('resideprovince' => $post['province'], 'residecity' => $post['city'], 'residedist' => $post['district']), array('uid' => $uid));
+			if($users_profile_exist) {
+				$result = pdo_update('users_profile', array('resideprovince' => $post['province'], 'residecity' => $post['city'], 'residedist' => $post['district']), array('uid' => $uid));
+			}else {
+				$data = array(
+						'uid' => $uid,
+						'createtime' => TIMESTAMP,
+						'resideprovince' => $post['province'],
+						'residecity' => $post['city'],
+						'residedist' => $post['district']
+					);
+				$result = pdo_insert('users_profile', $data);
+			}
 			break;
 	}
 	if($result) {
