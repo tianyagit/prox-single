58dd5189cb936fa87f7087ee18e43cfce9cc75c0
yangfeng <117635341@qq.com>
2016-12-27 17:15:21 +0800

    删除微擎卡券的manifest

---
+++ upgrade/20161227/upgrade.php
@@ -0,0 +1,7 @@
+<?php
+define('IN_SYS', true);
+require '../../framework/bootstrap.inc.php';
+require IA_ROOT . '/web/common/bootstrap.sys.inc.php';
+require IA_ROOT . '/web/common/common.func.php';
+
+unlink(IA_ROOT . '/addons/we7_coupon/manifest.xml');
