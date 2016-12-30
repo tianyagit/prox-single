89240644987d73ebd560e5520839d29efe1f67bf
cj <12620032227@qq.com>
2016-12-30 09:56:19 +0800

    二维码权限判断

--- web/source/platform/__init.php
+++ web/source/platform/__init.php
@@ -7,5 +7,12 @@ if ($action != 'material_post') {
 	define('FRAME', 'account');
 }
 if ($action == 'qr') {
-	define('FRAME', 'qr');
+	$platform_qr_permission =  uni_user_permission_check('platform_qr', false);
+	if ($platform_qr_permission ===  false) {
+		header("Location: ". url('platform/url2qr'));
+	}
+}
+
+if ($action == 'url2qr') {
+	define('ACTIVE_FRAME_URL', url('platform/qr'));
 }
