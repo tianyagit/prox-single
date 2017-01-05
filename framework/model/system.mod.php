c7cf706e5d0f8def52b86963c09c67e92730ff09
RenChao <donknap@gmail.com>
2017-01-05 18:10:28 +0800

    用户设置权限

--- framework/model/system.mod.php
+++ framework/model/system.mod.php
@@ -3,3 +3,17 @@
  * [WeEngine System] Copyright (c) 2013 WE7.CC
  */
 defined('IN_IA') or exit('Access Denied');
+
+/**
+ * 获取包括系统及模块所有的菜单权限
+ * 
+ */
+function system_menu_permission_list() {
+	cache_build_frame_menu();
+	$system_menu = cache_load('system_frame');
+	if(empty($system_menu)) {
+		cache_build_frame_menu();
+		$system_menu = cache_load('system_frame');
+	}
+	return $system_menu;
+}
