6ccbe6b864ca8076e7b223f439848f416f1e8392
yangfeng <117635341@qq.com>
2017-02-08 10:38:43 +0800

    会员卡设置增加会员等级显示和颜色设置

---
+++ upgrade/20170208/upgrade.php
@@ -0,0 +1,35 @@
+<?php
+define('IN_SYS', true);
+require '../../framework/bootstrap.inc.php';
+require IA_ROOT . '/web/common/bootstrap.sys.inc.php';
+require IA_ROOT . '/web/common/common.func.php';
+
+//会员卡数据修复
+$setting = pdo_getall('mc_card', '', '', 'id');
+if (!empty($setting)) {
+	foreach ($setting as $k => &$val) {
+		$color = iunserializer($val['color']);
+		$colors[] = iunserializer($val['color']);
+		if (!empty($color)) {
+			if (!is_array($color)) {
+				$val['color'] = array(
+					'title' => $color,
+					'rank' => '#333',
+					'name' => '#333',
+					'number' => '#333',
+				);
+			} else {
+				$val['color'] = $color;
+			}
+		} else {
+			$val['color'] = array(
+				'title' => '#333',
+				'rank' => '#333',
+				'name' => '#333',
+				'number' => '#333',
+			);
+		}
+		$update['color'] = iserializer($val['color']);
+		pdo_update('mc_card', $update, array('id' => $k));
+	}
+}
