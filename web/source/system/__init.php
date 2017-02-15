94db83012f4c8d39a85610fe4bab7027eaa5c185
yangfeng <117635341@qq.com>
2017-02-15 19:04:00 +0800

    代码优化

--- web/source/system/__init.php
+++ web/source/system/__init.php
@@ -10,6 +10,6 @@ if ($_GPC['account_type'] == ACCOUNT_TYPE_APP_NORMAL) {
 	define('ACCOUNT_TYPE', ACCOUNT_TYPE_APP_NORMAL);
 	define('ACCOUNT_TYPE_TEMPLATE', '-wxapp');
 } else {
-	define('ACCOUNT_TYPE', '');
+	define('ACCOUNT_TYPE', 0);
 	define('ACCOUNT_TYPE_TEMPLATE', '');
 }
