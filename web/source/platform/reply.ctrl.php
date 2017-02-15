2ecfd2dcc43d147af412472b96d086f9e47628d4
veking <594661320@qq.com>
2017-02-15 10:39:33 +0800

    修复模块参数设置无法保存问题 $module = WeUtility::createModule('core'); 改为 $module = WeUtility::createModule($m);

--- web/source/platform/reply.ctrl.php
+++ web/source/platform/reply.ctrl.php
@@ -194,7 +194,7 @@ if ($do == 'post') {
 			} else {
 				$rule['displayorder'] = range_limit($rule['displayorder'], 0, 254);
 			}
-			$module = WeUtility::createModule('core');
+			$module = WeUtility::createModule($m);
 			if (empty($module)) {
 				message('抱歉，模块不存在请重新选择其它模块！');
 			}
