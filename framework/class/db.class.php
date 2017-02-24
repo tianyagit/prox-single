a6474c8a67b921f50dda8efea01f71f2849a9d4e
RenChao <donknap@gmail.com>
2017-02-24 16:54:34 +0800

    整理

--- framework/class/db.class.php
+++ framework/class/db.class.php
@@ -632,7 +632,7 @@ class DB {
 					load()->web('common');
 					load()->web('template');
 				}
-				exit("SQL: <br/>{$append['sql']}<hr/>Params: <br/>{$params}<hr/>SQL Error: <br/>{$append['error'][2]}<hr/>Traces: <br/>{$ts}");
+				message("SQL: <br/>{$append['sql']}<hr/>Params: <br/>{$params}<hr/>SQL Error: <br/>{$append['error'][2]}<hr/>Traces: <br/>{$ts}", '', 'error');
 			}
 		}
 		return $this->errors;
