d95655c3ffd8b776fe7322d43198b45e12b7019c
brjun <351409246@qq.com>
2017-03-03 11:58:22 +0800

    更新升级文件

--- upgrade/20170302/upgrade.php
+++ upgrade/20170302/upgrade.php
@@ -13,7 +13,6 @@ if(!pdo_fieldexists('news_reply', 'media_id')) {
 }
 
 //自定义菜单为重名title添加后缀（如：title为‘默认菜单’的有两个重名，其id分别为1、2，执行该语句后title分别为：‘默认菜单_1’、‘默认菜单_2’）
-//title不为空时
+pdo_update('uni_account_menus', array('title' => '默认菜单'), array('title' => '', 'type' => 1));
+pdo_update('uni_account_menus', array('title' => '个性化菜单'), array('title' => '', 'type' => 3));
 pdo_query("UPDATE ". tablename('uni_account_menus') . " SET `title` = concat(title,'_',id) WHERE  `title` IN (SELECT a.`title` FROM (SELECT `title` FROM " . tablename('uni_account_menus') . "WHERE title <> '' GROUP BY `title` having count(*) >1 ) a)");
-//title为空时
-pdo_query("UPDATE ". tablename('uni_account_menus') . " SET `title` = concat('默认菜单','_',id) WHERE  `title` IN (SELECT a.`title` FROM (SELECT `title` FROM " . tablename('uni_account_menus') . "WHERE title = '' GROUP BY `title` having count(*) >1 ) a)");
