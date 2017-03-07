1c8901ce1ee410b5de74822ee768c41a525a28f7
RenChao <donknap@gmail.com>
2017-03-07 18:58:26 +0800

    account_type 为空时未处理

--- web/source/account/__init.php
+++ web/source/account/__init.php
@@ -16,7 +16,7 @@ if ($_GPC['account_type'] == ACCOUNT_TYPE_APP_NORMAL) {
 	define('ACCOUNT_TYPE_OFFCIAL', 0);
 	define('ACCOUNT_TYPE_NAME', '小程序');
 	define('ACCOUNT_TYPE_TEMPLATE', '-wxapp');
-} elseif ($_GPC['account_type'] == ACCOUNT_TYPE_OFFCIAL_NORMAL || $_GPC['account_type'] == ACCOUNT_TYPE_OFFCIAL_AUTH) {
+} elseif (empty($_GPC['account_type']) || $_GPC['account_type'] == ACCOUNT_TYPE_OFFCIAL_NORMAL || $_GPC['account_type'] == ACCOUNT_TYPE_OFFCIAL_AUTH) {
 	define('ACCOUNT_TYPE', ACCOUNT_TYPE_OFFCIAL_NORMAL);
 	$account_type_offcial = $_GPC['account_type'] == ACCOUNT_TYPE_OFFCIAL_NORMAL ? ACCOUNT_TYPE_OFFCIAL_NORMAL : ACCOUNT_TYPE_OFFCIAL_AUTH;
 	define('ACCOUNT_TYPE_OFFCIAL', $account_type_offcial);
