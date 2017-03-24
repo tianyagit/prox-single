0d6d4e0c3b22020ca78d796a27f8a94afc1ee2cc
cj <12620032227@qq.com>
2017-03-24 19:34:14 +0800

    0.8修复add_member漏洞

--- web/source/mc/member.ctrl.php
+++ web/source/mc/member.ctrl.php
@@ -298,8 +298,9 @@ if($do == 'del') {
 if($do == 'add') {
 	if($_W['isajax']) {
 		$type = trim($_GPC['type']);
+		$type_list = array('realname', 'mobile', 'password', 'email', 'credit1', 'credit2');
 		$data = trim($_GPC['data']);
-		if(empty($data) || empty($type)) {
+		if(empty($data) || empty($type) || !in_array($type, $type_list)) {
 			exit(json_encode(array('valid' => false)));
 		}
 		$user = pdo_get('mc_members', array('uniacid' => $_W['uniacid'], $type => $data));
