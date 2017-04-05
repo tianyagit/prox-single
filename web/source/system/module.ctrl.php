51dc4baf75aae9af5e2ee55f8bd26b8aa2586ec5
cj <12620032227@qq.com>
2017-04-05 15:02:21 +0800

    插件额删除安装

--- web/source/system/module.ctrl.php
+++ web/source/system/module.ctrl.php
@@ -313,8 +313,6 @@ if ($do =='install') {
 			}
 		}
 	}
-	unset($module['page']);
-	unset($module['supports']);
 	$module['permissions'] = iserializer($module['permissions']);
 	$module_subscribe_success = true;
 	if (!empty($module['subscribes'])) {
@@ -328,6 +326,12 @@ if ($do =='install') {
 	}
 	$pinyin = new Pinyin_Pinyin();
 	$module['title_initial'] = $pinyin->get_first_char($module['title']);
+	if (!empty($module['plugin']) && is_array($module['plugin'])) {
+		foreach ($module['plugin'] as $plugin) {
+			pdo_insert('module_plugin', array('name' => $plugin, 'main_module' => $module['name']));
+		}
+	}
+	unset($module['page'], $module['supports'], $module['plugin'], $module['main_module']);
 	if (pdo_insert('modules', $module)) {
 		if (strexists($manifest['install'], '.php')) {
 			if (file_exists($module_path . $manifest['install'])) {
