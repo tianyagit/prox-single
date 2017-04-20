c83499839968368540c311d9f9d0816066786d55
RenChao <donknap@gmail.com>
2017-04-20 09:47:01 +0800

    修复各别用户unserializer函数兼容，使用系统函数

--- web/source/profile/payment.ctrl.php
+++ web/source/profile/payment.ctrl.php
@@ -24,7 +24,7 @@ if(!empty($uniaccounts)) {
 	foreach($uniaccounts as $uniaccount) {
 		$account = account_fetch($uniaccount['default_acid']);
 		$account_setting = pdo_get('uni_settings', array('uniacid' => $account['uniacid']));
-		$payment = iunserializer($account_setting['payment']);
+		$payment = (array)unserialize($account_setting['payment']);
 		if (!empty($account['key']) && !empty($account['secret']) && in_array ($account['level'], array (4)) && !empty($payment) && intval($payment['wechat']['switch']) == 1) {
 			if ((!is_bool($payment['wechat']['switch']) && $payment['wechat']['switch'] != 4) || (is_bool($payment['wechat']['switch']) && !empty($payment['wechat']['switch']))) {
 				$borrow[$account['uniacid']] = $account['name'];
