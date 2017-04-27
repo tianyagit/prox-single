53148f56e93789c922fd488593d7fac8b5f5c3ae
hdd <460471121@qq.com>
2017-04-27 19:20:50 +0800

    验证链接

--- framework/model/material.mod.php
+++ framework/model/material.mod.php
@@ -95,8 +95,11 @@ function material_news_set($data, $attach_id) {
 	global $_W;
 	$attach_id = intval($attach_id);
 	foreach ($data as $key => $news) {
-		if (empty($news['title']) || (!empty($news['thumb']) && !parse_path($news['thumb'])) || (!empty($news['url']) && !parse_path($news['url'])) || (!empty($news['content_source_url']) && !parse_path($news['content_source_url']))){
+		if (empty($news['title'])){
 			return error('-1', '参数有误');
+		}
+		if (!material_url_check($news['content_source_url']) || !material_url_check($news['url']) || !material_url_check($news['thumb'])){
+			return error('-3', '提交链接参数不合法');
 		}
 		$post_news[] = array(
 			'id'				=> isset($news['id'])? intval($news['id']) : '',
@@ -108,9 +111,9 @@ function material_news_set($data, $attach_id) {
 			'content' 			=> htmlspecialchars_decode($news['content']),
 			'url' 				=> $news['url'],
 			'show_cover_pic' 	=> $news['show_cover_pic'] ? 1 : 0,
-			'displayorder' 		=> $key,
+			'displayorder' 		=> intval($key),
 			'thumb_media_id' 	=> isset($news['media_id'])? addslashes($news['media_id']) : '',
-			'content_source_url' => preg_replace('/(http|https):\/\/.\/index.php/', './index.php', $news['content_source_url']),
+			'content_source_url' => $news['content_source_url'],
 		);
 	}
 	if ($attach_id > 0){
@@ -508,3 +511,17 @@ function material_delete($material_id, $location){
 	return $result;
 }
 
+/**
+ * 验证输入内容是否为合法链接
+ * @param $str
+ * @return boolean
+ */
+function material_url_check($str){
+	if (empty($str)){
+		return true;
+	}else{
+		$preg1 = "/(http|https):\/\/*/";
+		$preg2 = "/.\/index.php\?*/";
+		return preg_match($preg1, $str) || preg_match($preg2, $str);
+	}
+}
