4db2d055be5977235cd2188a21618d37a7117cc3
brjun <351409246@qq.com>
2017-05-25 21:43:53 +0800

    新版：整理代码

--- upgrade/20170525/upgrade.php
+++ upgrade/20170525/upgrade.php
@@ -1,15 +1,13 @@
 <?php
 /**
- * 微擎1.0缓存表key的长度问题
+ * 更新rule表containtype字段数据：image换成images（与1.0之前版本统一）
  * [WeEngine System] Copyright (c) 2013 WE7.CC
  */
 
 define('IN_SYS', true);
 require '../../framework/bootstrap.inc.php';
 
-//更新rule表containtype字段数据：image换成images（与1.0之前版本统一）
 $getall_containtype_data = pdo_getall('rule', array('containtype <>' => ''));
-
 foreach ($getall_containtype_data as $containtype_val) {
 	$types = explode(',', $containtype_val['containtype']);
 	if (in_array('image', $types)) {
