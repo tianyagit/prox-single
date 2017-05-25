cc51a4ab887643fa2249129d6422ad9827fb7eb6
brjun <351409246@qq.com>
2017-05-25 21:43:03 +0800

    新版：自定义回复图片问题（8646）

---
+++ upgrade/20170525/upgrade.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * 微擎1.0缓存表key的长度问题
+ * [WeEngine System] Copyright (c) 2013 WE7.CC
+ */
+
+define('IN_SYS', true);
+require '../../framework/bootstrap.inc.php';
+
+//更新rule表containtype字段数据：image换成images（与1.0之前版本统一）
+$getall_containtype_data = pdo_getall('rule', array('containtype <>' => ''));
+
+foreach ($getall_containtype_data as $containtype_val) {
+	$types = explode(',', $containtype_val['containtype']);
+	if (in_array('image', $types)) {
+		$new_containtype_val = str_replace('image', 'images', $containtype_val['containtype']);
+		pdo_update('rule', array('containtype' => $new_containtype_val), array('id' => $containtype_val['id']));
+	}
+}
