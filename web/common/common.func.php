35e246132d70a0bb36b1555b302733a0dafb21ee
wjw353830223 <353830223@qq.com>
2017-06-21 20:03:06 +0800

    mantis 8727 系统管理首页

--- web/common/common.func.php
+++ web/common/common.func.php
@@ -577,3 +577,63 @@ EOF;
 	}
 	return '';
 }
+/**
+ * 数据库备份信息
+ * @param string $path 备份文件路径
+ * @return array;
+ */
+function database_reduction ($path) {
+	$reduction = array();
+	if (!is_dir($path)) {
+		return array();
+	}
+	if ($handle = opendir($path)) {
+		while (false !== ($bakdir = readdir($handle))) {
+			if ($bakdir == '.' || $bakdir == '..') {
+				continue;
+			}
+			if (preg_match('/^(?P<time>\d{10})_[a-z\d]{8}$/i', $bakdir, $match)) {
+				$time = $match['time'];
+				//获取随机字符串
+				if ($handle1= opendir($path . $bakdir)) {
+					while (false !== ($filename = readdir($handle1))) {
+						if ($filename == '.' || $filename == '..') {
+							continue;
+						}
+						if (preg_match('/^volume-(?P<prefix>[a-z\d]{10})-\d{1,}\.sql$/i', $filename, $match1)) {
+							$volume_prefix = $match1['prefix'];
+							if (!empty($volume_prefix)) {
+								break;
+							}
+						}
+					}
+				}
+				for ($i = 1;;) {
+					$last = $path . $bakdir . "/volume-{$volume_prefix}-{$i}.sql";
+					$i++;
+					$next = $path . $bakdir . "/volume-{$volume_prefix}-{$i}.sql";
+					if (!is_file($next)) {
+						break;
+					}
+				}
+				if (is_file($last)) {
+					$fp = fopen($last, 'r');
+					fseek($fp, -27, SEEK_END);
+					$end = fgets($fp);
+					fclose($fp);
+					if ($end == '----WeEngine MySQL Dump End') {
+						$row = array(
+								'bakdir'=> $bakdir,
+								'time'=> $time,
+								'volume'=> $i - 1
+						);
+						$reduction[$bakdir] = $row;
+						continue;
+					}
+				}
+			}
+			rmdirs($path . $bakdir);
+		}
+	}
+	return $reduction;
+}
