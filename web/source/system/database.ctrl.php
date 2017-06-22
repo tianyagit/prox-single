4556b837d88ab7d85bb0535eab75814e558bbef7
wjw353830223 <353830223@qq.com>
2017-06-22 11:45:25 +0800

    优化

--- web/source/system/database.ctrl.php
+++ web/source/system/database.ctrl.php
@@ -11,7 +11,6 @@ load()->func('file');
 load()->model('cloud');
 load()->func('db');
 load()->model('system');
-
 $dos = array('backup', 'restore', 'trim', 'optimize', 'run');
 $do = in_array($do, $dos) ? $do : 'backup';
 
