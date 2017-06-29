3f6b1197b262ec52a453b340be7f400ee8935d5a
cj <12620032227@qq.com>
2017-06-29 18:14:27 +0800

    优化判断是否手机登录代码

--- framework/class/agent.class.php
+++ framework/class/agent.class.php
@@ -160,7 +160,9 @@ class Agent
 		// 如果via信息含有wap则一定是移动设备,部分服务商会屏蔽该信息
 		if (isset($_SERVER['HTTP_VIA'])) {
 			// 找不到为flase,否则为true
-			return stristr($_SERVER['HTTP_VIA'], "wap") ? true : false;
+			if (stristr($_SERVER['HTTP_VIA'], "wap")) {
+				return true;
+			}
 		}
 		// 脑残法，判断手机发送的客户端标志,兼容性有待提高
 		if (isset ($_SERVER['HTTP_USER_AGENT'])) {
