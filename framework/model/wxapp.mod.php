c6d35ab5b3342d26c0e0556cc2079498b4c76c2c
malina <1643827412@qq.com>
2017-07-03 09:26:04 +0800

    关联人必须是对应公众号的主管理员

--- framework/model/wxapp.mod.php
+++ framework/model/wxapp.mod.php
@@ -363,7 +363,7 @@ function wxapp_search_link_account($module_name = '') {
 	if (!empty($owned_account)) {
 		foreach ($owned_account as $key => $account) {
 			$account['role'] = uni_permission($_W['uid'], $account['uniacid']);
-			if (!in_array($account['role'], array(ACCOUNT_MANAGE_NAME_OWNER, ACCOUNT_MANAGE_NAME_FOUNDER))) {
+			if ($account['role'] != ACCOUNT_MANAGE_NAME_OWNER) {
 				unset($owned_account[$key]);
 			}
 		}
