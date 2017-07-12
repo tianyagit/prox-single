48a97a0cac804f5a39f93ee4db73d5bc1253f1c8
brjun <351409246@qq.com>
2017-07-12 16:39:21 +0800

    新版：修复除创始人外，其他人素材删除失败的问题

--- framework/model/material.mod.php
+++ framework/model/material.mod.php
@@ -96,9 +96,9 @@ function material_news_set($data, $attach_id) {
 	global $_W;
 	$attach_id = intval($attach_id);
 	foreach ($data as $key => $news) {
-		if (empty($news['title']) || 
-			(!empty($news['thumb']) && !parse_path($news['thumb'])) || 
-			(!empty($news['url']) && !parse_path($news['url'])) || 
+		if (empty($news['title']) ||
+			(!empty($news['thumb']) && !parse_path($news['thumb'])) ||
+			(!empty($news['url']) && !parse_path($news['url'])) ||
 			(!empty($news['content_source_url']) && !parse_path($news['content_source_url']))
 		) {
 			return error('-1', '参数有误');
@@ -169,7 +169,7 @@ function material_get($attach_id) {
 		$material = pdo_get('wechat_attachment', array('id' => $attach_id));
 	} else {
 		$media_id = trim($attach_id);
-		$material = pdo_get('wechat_attachment', array('media_id' => $media_id)); 
+		$material = pdo_get('wechat_attachment', array('media_id' => $media_id));
 	}
 	if (!empty($material)) {
 		if ($material['type'] == 'news') {
@@ -368,7 +368,7 @@ function material_local_news_upload($attach_id) {
  * 目前图片、音频、视频素材上传都用这个方法
  * 上传素材文件到微信，获取mediaId
  * @param string $url
- * 
+ *
  */
 function material_local_upload_by_url($url, $type='images') {
 	global $_W;
@@ -430,7 +430,7 @@ function material_upload_limit() {
  * 删除图文素材
  * @param int $material_id 素材id
  * @type string $type 素材类型
- * 
+ *
  */
 function material_news_delete($material_id){
 	global $_W;
@@ -438,7 +438,7 @@ function material_news_delete($material_id){
 	if (is_error($permission)) {
 		return error(-1, $permission['message']);
 	}
-	if (empty($_W['isfounder']) && !empty($permission) && !in_array('platform_material', $permission)) {
+	if (empty($_W['isfounder']) && !empty($permission) && $permission[0] != 'all' && !in_array('platform_material', $permission)) {
 		return error('-1', '您没有权限删除该文件');
 	}
 	$material_id = intval($material_id);
