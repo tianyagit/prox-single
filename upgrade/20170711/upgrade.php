7dcdd33db8944d681947c6548dc610507a74d557
malina <1643827412@qq.com>
2017-07-14 20:46:37 +0800

    优化  副站长

--- upgrade/20170711/upgrade.php
+++ upgrade/20170711/upgrade.php
@@ -7,14 +7,14 @@
 define('IN_SYS', true);
 require '../../framework/bootstrap.inc.php';
 if (!pdo_fieldexists('users', 'founder_groupid')) {
-	pdo_query("ALTER TABLE tablename('users') ADD COLUMN `founder_groupid` TINYINT(4) NOT NULL DEFAULT 0  COMMENT '管理组，1是创始人，2是副创始人' AFTER `groupid`;");
+	pdo_query("ALTER TABLE " .tablename('users') . " ADD COLUMN `founder_groupid` TINYINT(4) NOT NULL DEFAULT 0  COMMENT '管理组，1是创始人，2是副创始人' AFTER `groupid`;");
 }
 if (!pdo_fieldexists('users', 'vice_founder_id')) {
-	pdo_query("ALTER TABLE tablename('users') ADD COLUMN `vice_founder_id` int(10) NOT NULL DEFAULT 0 COMMENT '副创始人uid' AFTER `uid`;");
+	pdo_query("ALTER TABLE " . tablename('users') . " ADD COLUMN `vice_founder_id` int(10) NOT NULL DEFAULT 0 COMMENT '副创始人uid' AFTER `uid`;");
 }
 if (!pdo_fieldexists('uni_group', 'vice_founder_id')) {
-	pdo_query("ALTER TABLE tablename('uni_group') ADD COLUMN `vice_founder_id` int(10) NOT NULL DEFAULT 0 COMMENT '副创始人uid' AFTER `id`;");
+	pdo_query("ALTER TABLE " . tablename('uni_group') . " ADD COLUMN `vice_founder_id` int(10) NOT NULL DEFAULT 0 COMMENT '副创始人uid' AFTER `id`;");
 }
 if (!pdo_fieldexists('users_group', 'vice_founder_id')) {
-	pdo_query("ALTER TABLE tablename('users_group') ADD COLUMN `vice_founder_id` int(10) NOT NULL DEFAULT 0 COMMENT '副创始人uid' AFTER `id`;");
+	pdo_query("ALTER TABLE " . tablename('users_group') . " ADD COLUMN `vice_founder_id` int(10) NOT NULL DEFAULT 0 COMMENT '副创始人uid' AFTER `id`;");
 }
