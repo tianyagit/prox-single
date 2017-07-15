fbc10e54329c720940374b4e9e002b09c9d186e0
cj <12620032227@qq.com>
2017-07-15 19:12:39 +0800

    微信退款

--- payment/wechat/refund.php
+++ payment/wechat/refund.php
@@ -45,16 +45,18 @@ $pay_setting['signkey'] = $pay_setting['version'] == 1 ? $pay_setting['key'] : $
 if(!empty($pay_setting['signkey'])) {
 	WeUtility::logging('refund', var_export($wechat_data, true));
 	$key = md5($pay_setting['signkey']);
-	$refund = aes_encode($wechat_data['req_info'], $pay_setting['signkey'], $wechat_data['appid']);
+	$wechat_data['req_info'] = aes_pkcs7_decode($wechat_data['req_info'], $key);
+	$refund = json_decode(json_encode(isimplexml_load_string($wechat_data['req_info'], 'SimpleXMLElement', LIBXML_NOCDATA)), true);
+
 	if(!empty($refund)) {
 		$pay_log = pdo_get('core_paylog', array('uniacid' => $_W['uniacid'], 'uniontid' => $refund['out_trade_no']));
 		$refund_log = pdo_get('core_refundlog', array('uniacid' => $_W['uniacid'], 'refund_uniontid' => $refund['out_refund_no'], 'uniontid' => $refund['out_trade_no']));
 		//此处判断微信请求消息金额必须与系统发起的金额一致
-		if(!empty($refund_log) && $refund_log['status'] == '0' && (($refund['total_fee'] / 100) == $refund_log['card_fee'])) {
+		if(!empty($refund_log) && $refund_log['status'] == '0' && (($refund['total_fee'] / 100) == $pay_log['card_fee'])) {
 			pdo_update('core_refundlog', array('status' => 1), array('id' => $refund_log['id']));
 			$site = WeUtility::createModuleSite($pay_log['module']);
 			if(!is_error($site)) {
-				$method = 'payResult';
+				$method = 'refundResult';
 				if (method_exists($site, $method)) {
 					$ret = array();
 					$ret['uniacid'] = $pay_log['uniacid'];
@@ -65,9 +67,9 @@ if(!empty($pay_setting['signkey'])) {
 					$ret['uniontid'] = $pay_log['uniontid'];
 					$ret['refund_uniontid'] = $refund_log['refund_uniontid'];
 					$ret['user'] = $pay_log['openid'];
-					$ret['fee'] = $wechat_data['fee'];
-					if(!empty($wechat_data['success_time'])) {
-						$ret['refund_time'] = strtotime($wechat_data['time_end']);
+					$ret['fee'] = $refund['fee'];
+					if(!empty($refund['success_time'])) {
+						$ret['refund_time'] = strtotime($refund['time_end']);
 					}
 					$site->$method($ret);
 					exit('success');
