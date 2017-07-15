01ea930dce8e735a43e1cdce647e9c833a7a9213
cj <12620032227@qq.com>
2017-07-15 16:54:25 +0800

    aes_pkcs7_decode（） 解密方法

--- framework/function/global.func.php
+++ framework/function/global.func.php
@@ -1202,8 +1202,16 @@ function aes_encode($message, $encodingaeskey = '', $appid = '') {
 	return $encrypt_msg;
 }
 
-function aes_pkcs7_decode() {
-	
+function aes_pkcs7_decode($encrypt_data, $key, $iv = false) {
+	require_once IA_ROOT . '/framework/library/pkcs7/pkcs7Encoder.php';
+	$encrypt_data = base64_decode($encrypt_data);
+	$iv = base64_decode($iv);
+	$pc = new Prpcrypt($key);
+	$result = $pc->decrypt($encrypt_data, $iv);
+	if ($result[0] != 0) {
+		return error($result[0], '解密失败');
+	}
+	return $result;
 }
 
 /*
