60c7c193e6cab2c98af95903c91efeb7653c633e
RenChao <donknap@gmail.com>
2017-07-19 14:55:18 +0800

    整理代码

--- web/source/article/notice.ctrl.php
+++ web/source/article/notice.ctrl.php
@@ -11,9 +11,6 @@ $dos = array('category_post', 'category', 'category_del', 'list', 'post', 'batch
 $do = in_array($do, $dos) ? $do : 'list';
 uni_user_permission_check('system_article_notice');
 
-if ($_W['user']['founder_groupid'] == ACCOUNT_MANAGE_GROUP_VICE_FOUNDER) {
-	itoast('无权限操作！', url('account/manage'), 'error');
-}
 //添加公告分类
 if ($do == 'category_post') {
 	$_W['page']['title'] = '公告分类-公告管理-文章-系统管理';
