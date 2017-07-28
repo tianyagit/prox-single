447fbb2cf62df0cb24259bf10806352e452dea3c
brjun <351409246@qq.com>
2017-07-28 11:01:21 +0800

    新版：修复会员中心diy部分手机端显示不全（9677）

--- app/common/template.func.php
+++ app/common/template.func.php
@@ -121,6 +121,7 @@ function template_compile($from, $to) {
 		mkdirs($path);
 	}
 	$content = template_parse(file_get_contents($from));
+	$content .= '<script type="text/javascript"> var scale = 1, marginLeft, marginTop; var width = window.screen.width; var height = window.screen.height; width / height > 320 / 568 ? (scale = height / 568,  marginLeft = (width / scale - 320) / 2, width = "320px") : (scale = width / 320, marginTop = (height / scale - 568) / 2, width = "100%"); window != window.top && $(".container").css({width: "100%", height: "100%", overflow: "hidden", "transform-origin": "top left", transform: "scale(" + scale + ")"}); $(".container div").eq(0).css({"width" : width, "marginTop" : marginTop, "marginLeft": marginLeft}); $("meta[name='."'".'viewport'."'".']").attr("content", "width=320, initial-scale=" + scale + ", maximum-scale=" + scale + ", user-scalable=no"); </script>';
 	file_put_contents($to, $content);
 }
 
