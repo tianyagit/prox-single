24bf260e2a720bf04d1b4aeff1c38a4de1236e93
brjun <351409246@qq.com>
2017-08-01 11:54:47 +0800

    新版：处理模块兼容问题（10075）

--- web/source/profile/__init.php
+++ web/source/profile/__init.php
@@ -1,9 +1,13 @@
 <?php
 /**
- * 
+ *
  * [WeEngine System] Copyright (c) 2013 WE7.CC
  */
 defined('IN_IA') or exit('Access Denied');
+
+if (strexists($_W['siteurl'], 'c=profile&a=module&do=setting')) {
+	itoast('', url('module/module-account/setting', array('m' => $_GPC['m'])), 'info');
+}
 
 define('FRAME', 'account');
 checkaccount();
