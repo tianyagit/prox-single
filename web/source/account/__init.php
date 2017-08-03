833f8d20813284f88f88f647258becd6dd10cd9a
malina <1643827412@qq.com>
2017-08-03 16:33:04 +0800

    0010087: 权限设置，小程序用户无法加到“附加权限”内

--- web/source/account/__init.php
+++ web/source/account/__init.php
@@ -17,10 +17,12 @@ if ($_GPC['account_type'] == ACCOUNT_TYPE_APP_NORMAL) {
 	define('ACCOUNT_TYPE_OFFCIAL', 0);
 	define('ACCOUNT_TYPE_NAME', '小程序');
 	define('ACCOUNT_TYPE_TEMPLATE', '-wxapp');
+	define('ACCOUNT_TYPE_SUPPORT', 'wxapp_support');
 } elseif (empty($_GPC['account_type']) || $_GPC['account_type'] == ACCOUNT_TYPE_OFFCIAL_NORMAL || $_GPC['account_type'] == ACCOUNT_TYPE_OFFCIAL_AUTH) {
 	define('ACCOUNT_TYPE', ACCOUNT_TYPE_OFFCIAL_NORMAL);
 	$account_type_offcial = $_GPC['account_type'] == ACCOUNT_TYPE_OFFCIAL_NORMAL ? ACCOUNT_TYPE_OFFCIAL_NORMAL : ACCOUNT_TYPE_OFFCIAL_AUTH;
 	define('ACCOUNT_TYPE_OFFCIAL', $account_type_offcial);
 	define('ACCOUNT_TYPE_NAME', '公众号');
 	define('ACCOUNT_TYPE_TEMPLATE', '');
+	define('ACCOUNT_TYPE_SUPPORT', 'app_support');
 }
