021c47ef7c7ed4c28ca6c767e63c56b6886b4f9a
RenChao <donknap@gmail.com>
2017-08-21 17:47:56 +0800

    服务对象，基本方案确定

---
+++ framework/service/account.service.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * 
+ * [WeEngine System] Copyright (c) 2013 WE7.CC
+ */
+ 
+defined('IN_IA') or exit('Access Denied');
+
+class AccountService extends We7Service {
+	
+	public function searchAccountList() {
+		return $this->query->from('uni_account', 'a')->leftjoin('account', 'b')->
+				on(array('a.uniacid' => 'b.uniacid', 'a.default_acid' => 'b.acid'))->getall();
+	}
+	
+	public function searchKeyword($title) {
+		$this->query->where('a.name LIKE', "%{$title}%");
+		return $this;
+	}
+}
