290e77023ff2ab13b8c4f90775e828846a6b141f
malina <1643827412@qq.com>
2017-10-28 12:03:31 +0800

    短息到期   后台

--- upgrade/1.6.1/2017_10_25_365567_upgrade_users_profile.php
+++ upgrade/1.6.1/2017_10_25_365567_upgrade_users_profile.php
@@ -15,23 +15,6 @@ class UpgradeUsersProfile {
 	 *  执行更新
 	 */
 	public function up() {
-		$sms_info = pdo_get('core_cron', array('filename' => 'sms'));
-		if (empty($sms_info)) {
-			load()->model('cloud');
-			$data = array(
-					'module' => 'task',
-					'type' => 2,
-					'name' => '用户到期发送短信任务',
-					'filename' => 'sms',
-					'day' => -1,
-					'hour' => 16,
-					'minute' => 30,
-					'status' => 1,
-					'createtime' =>TIMESTAMP
-			);
-			cron_add($data);
-		}
-
 		if (!pdo_fieldexists('users_profile', 'is_send_mobile_status')) {
 			pdo_query('ALTER TABLE ' . tablename('users_profile') . " ADD `is_send_mobile_status` TINYINT(3) NOT NULL DEFAULT 0 COMMENT '0没发短信，1已经发送信息';");
 		}
