2399adf3087c13923919de826d7d0bde71b5e1d7
brjun <brj1001@163.com>
2017-11-03 17:53:10 +0800

    删除冗余代码

--- web/source/user/register.ctrl.php
+++ web/source/user/register.ctrl.php
@@ -83,8 +83,7 @@ if(checksubmit()) {
 			$profile['createtime'] = TIMESTAMP;
 			pdo_insert('users_profile', $profile);
 		}
-		pdo_update('users_invitation', array('inviteuid' => $uid), array('id' => $invite['id']));
-		itoast('注册成功'.(!empty($_W['setting']['register']['verify']) ? '，請等待管理员审核！' : '，请重新登录！'), url('user/login', array('uid' => $uid, 'username' => $member['username'])), 'success');
+		itoast('注册成功'.(!empty($_W['setting']['register']['verify']) ? '，请等待管理员审核！' : '，请重新登录！'), url('user/login', array('uid' => $uid, 'username' => $member['username'])), 'success');
 	}
 	itoast('增加用户失败，请稍候重试或联系网站管理员解决！', '', '');
 }
