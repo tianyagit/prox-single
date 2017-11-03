9dbf65d1cd9dd730de9b5dbdc92b2af5e6d7d8c2
brjun <brj1001@163.com>
2017-11-03 19:53:46 +0800

    店员菜单界面（11902）

--- web/common/common.func.php
+++ web/common/common.func.php
@@ -267,15 +267,11 @@ function buildframes($framename = ''){
 	}
 	//从数据库中获取用户权限，并附加上系统管理中的权限
 	//仅当系统管理时才使用预设权限
-	if (!empty($_W['role']) && ($_W['role'] == ACCOUNT_MANAGE_NAME_OPERATOR || $_W['role'] == ACCOUNT_MANAGE_NAME_MANAGER || $_W['role'] == ACCOUNT_MANAGE_NAME_OWNER)) {
+	if (!empty($_W['role']) && !user_is_founder($_W['uid'])) {
 		$user_permission = permission_account_user('system');
 	}
 	if (empty($_W['role']) && empty($_W['uniacid'])) {
 		$user_permission = permission_account_user('system');
-	}
-	//@@todo 店员界面菜单
-	if (!empty($_W['role']) && $_W['role'] == 'clerk') {
-
 	}
 	//系统公众号菜单权限
 	if (!empty($user_permission)) {
@@ -602,7 +598,9 @@ function frames_menu_append() {
 			'system_my',
 			'system_setting_updatecache',
 		),
-		'clerk' => array(),
+		'clerk' => array(
+			'system_my',
+		),
 	);
 	return $system_menu_default_permission;
 }
