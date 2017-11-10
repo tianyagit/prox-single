14af0ee89f93eb40ef9fe5519073ff11318a2e24
brjun <brj1001@163.com>
2017-11-10 14:05:45 +0800

    修复系统自动同步粉丝信息只能同步10条并非全部数据的问题

--- web/source/utility/sync.ctrl.php
+++ web/source/utility/sync.ctrl.php
@@ -11,7 +11,7 @@ if(!empty($_W['uniacid'])) {
 	$setting = uni_setting($_W['uniacid'], array('sync'));
 	$sync_setting = $setting['sync'];
 	if($sync_setting == 1 && $_W['account']['type'] == 1 && $_W['account']['level'] >= ACCOUNT_TYPE_OFFCIAL_AUTH) {
-		$fans = pdo_getall('mc_mapping_fans', array('uniacid' => $_W['uniacid'], 'follow' => 1), array('fanid', 'openid', 'acid', 'uid', 'uniacid'), 'fanid', 'fanid DESC', '10');
+		$fans = pdo_getall('mc_mapping_fans', array('uniacid' => $_W['uniacid'], 'follow' => 1), array('fanid', 'openid', 'acid', 'uid', 'uniacid'), 'fanid', array('updatetime ASC', 'fanid DESC'), '10');
 		if(!empty($fans)) {
 			foreach($fans as $row) {
 				mc_init_fans_info($row['openid']);
