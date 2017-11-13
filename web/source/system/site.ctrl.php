40988bf8d399b6c974a3c428c3718697d4edafe2
malina <1643827412@qq.com>
2017-11-13 15:07:40 +0800

    防挂马  备案

--- web/source/system/site.ctrl.php
+++ web/source/system/site.ctrl.php
@@ -21,7 +21,7 @@ if ($do == 'copyright') {
 		$data = array(
 			'status' => $_GPC['status'],
 			'reason' => $_GPC['reason'],
-			'icp' => $_GPC['icp'],
+			'icp' => strip_tags(htmlspecialchars_decode($_GPC['icp'])),
 		);
 		$test = setting_save($data, 'copyright');
 		itoast('更新设置成功！', url('system/site'), 'success');
