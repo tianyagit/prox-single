b59df83922d86bc4e4db1821b98c68b876d19f4d
cj <12620032227@qq.com>
2017-11-16 17:56:32 +0800

    删除模块更新云缓存问题

--- web/source/system/updatecache.ctrl.php
+++ web/source/system/updatecache.ctrl.php
@@ -7,17 +7,24 @@ defined('IN_IA') or exit('Access Denied');
 
 load()->model('cache');
 load()->model('setting');
+load()->object('cloudapi');
 
 $_W['page']['title'] = '更新缓存 - 设置 - 系统管理';
 
 //清空缓存分为两种，一种为重建，一种为清空。
 //清空类的直接把缓存全部删除，不在一条一条的删除
 if (checksubmit('submit', true)) {
+	$cloud_api = new CloudApi();
+	$cloud_cache_key = array(
+		'key' => array(cache_system_key('module:all_uninstall'), cache_system_key('user_modules:' . $_W['uid']))
+	);
+	$cloud_api->post('cache', 'delete', $cloud_cache_key);
 	$account_ticket_cache = cache_read('account:ticket');
 	pdo_delete('core_cache');
 	cache_clean();
 	cache_write('account:ticket', $account_ticket_cache);
 	unset($account_ticket_cache);
+
 	cache_build_template();
 	cache_build_users_struct();
 	cache_build_module_status();
