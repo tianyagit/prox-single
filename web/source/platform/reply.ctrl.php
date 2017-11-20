1acc758a705fea7a09ebdd456266b221114e34d8
brjun <brj1001@163.com>
2017-11-20 11:47:35 +0800

    修复非文字自动回复保存不生效的问题（12222）

--- web/source/platform/reply.ctrl.php
+++ web/source/platform/reply.ctrl.php
@@ -308,7 +308,7 @@ if ($do == 'post') {
 			if ($reply_type == 'module') {
 				$setting[$type] = array('type' => 'module', 'module' => $module);
 			} else {
-				$rule = pdo_get('rule_keyword', array('rid' => $rule_id, 'uniacid' => $_W['uniacid']));
+				$rule = pdo_get('rule_keyword', array('id' => $rule_id, 'uniacid' => $_W['uniacid']));
 				$setting[$type] = array('type' => 'keyword', 'keyword' => $rule['content']);
 			}
 			uni_setting_save('default_message', $setting);
