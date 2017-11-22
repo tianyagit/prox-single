388d3e81a8cbfd72972fcfab8c17d77191cc7494
RenChao <donknap@gmail.com>
2017-11-22 15:52:28 +0800

    整 理代码

--- framework/function/global.func.php
+++ framework/function/global.func.php
@@ -1399,49 +1399,6 @@ function check_url_not_outside_link($redirect) {
 	return $redirect;
 }
 
-/**
- *  去掉可能造成xss攻击的字符
- * @param $val $string 需处理的字符串
- */
-function remove_xss($val) {
-	$val = preg_replace('/([\x0e-\x19])/', '', $val);
-	$search = 'abcdefghijklmnopqrstuvwxyz';
-	$search .= 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-	$search .= '1234567890!@#$%^&*()';
-	$search .= '~`";:?+/={}[]-_|\'\\';
-	for ($i = 0; $i < strlen($search); $i++) {
-		$val = preg_replace('/(&#[xX]0{0,8}'.dechex(ord($search[$i])).';?)/i', $search[$i], $val);
-		$val = preg_replace('/(&#0{0,8}'.ord($search[$i]).';?)/', $search[$i], $val);
-	}
-	$ra1 = array('javascript', 'vbscript', 'expression', 'applet', 'meta', 'xml', 'blink', 'link', 'script', 'embed', 'object', 'frameset', 'ilayer', 'bgsound', 'title', 'base');
-	$ra2 = array('onabort', 'onactivate', 'onafterprint', 'onafterupdate', 'onbeforeactivate', 'onbeforecopy', 'onbeforecut', 'onbeforedeactivate', 'onbeforeeditfocus', 'onbeforepaste', 'onbeforeprint', 'onbeforeunload', 'onbeforeupdate', 'onblur', 'onbounce', 'oncellchange', 'onchange', 'onclick', 'oncontextmenu', 'oncontrolselect', 'oncopy', 'oncut', 'ondataavailable', 'ondatasetchanged', 'ondatasetcomplete', 'ondblclick', 'ondeactivate', 'ondrag', 'ondragend', 'ondragenter', 'ondragleave', 'ondragover', 'ondragstart', 'ondrop', 'onerror', 'onerrorupdate', 'onfilterchange', 'onfinish', 'onfocus', 'onfocusin', 'onfocusout', 'onhelp', 'onkeydown', 'onkeypress', 'onkeyup', 'onlayoutcomplete', 'onload', 'onlosecapture', 'onmousedown', 'onmouseenter', 'onmouseleave', 'onmousemove', 'onmouseout', 'onmouseover', 'onmouseup', 'onmousewheel', 'onmove', 'onmoveend', 'onmovestart', 'onpaste', 'onpropertychange', 'onreadystatechange', 'onreset', 'onresize', 'onresizeend', 'onresizestart', 'onrowenter', 'onrowexit', 'onrowsdelete', 'onrowsinserted', 'onscroll', 'onselect', 'onselectionchange', 'onselectstart', 'onstart', 'onstop', 'onsubmit', 'onunload', 'import');
-	$ra = array_merge($ra1, $ra2);
-	$found = true;
-	while ($found == true) {
-		$val_before = $val;
-		for ($i = 0; $i < sizeof($ra); $i++) {
-			$pattern = '/';
-			for ($j = 0; $j < strlen($ra[$i]); $j++) {
-				if ($j > 0) {
-					$pattern .= '(';
-					$pattern .= '(&#[xX]0{0,8}([9ab]);)';
-					$pattern .= '|';
-					$pattern .= '|(&#0{0,8}([9|10|13]);)';
-					$pattern .= ')*';
-				}
-				$pattern .= $ra[$i][$j];
-			}
-			$pattern .= '/i';
-			$replacement = substr($ra[$i], 0, 2).'<x>'.substr($ra[$i], 2);
-			$val = preg_replace($pattern, $replacement, $val);
-			if ($val_before == $val) {
-				$found = false;
-			}
-		}
-	}
-	return $val;
-}
-
 function set_attach_url() {
 	global $_W;
 	$_W['attachurl'] = $_W['attachurl_local'] = $_W['siteroot'] . $_W['config']['upload']['attachdir'] . '/';
@@ -1462,6 +1419,3 @@ function set_attach_url() {
 	}
 	return true;
 }
-
-
-
