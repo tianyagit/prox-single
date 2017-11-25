d572ff4ecfeb37137f38980b7c737c554d31f691
RenChao <donknap@gmail.com>
2017-11-25 10:25:44 +0800

    整理global代码

--- framework/function/global.func.php
+++ framework/function/global.func.php
@@ -1216,27 +1216,6 @@ function iarray_change_key_case($array, $case = CASE_LOWER){
 	return $array;
 }
 
-function strip_gpc($values, $type = 'g') {
-	$filter = array(
-		'g' => "'|(and|or)\\b.+?(>|<|=|in|like)|\\/\\*.+?\\*\\/|<\\s*script\\b|\\bEXEC\\b|UNION.+?SELECT|UPDATE.+?SET|INSERT\\s+INTO.+?VALUES|(SELECT|DELETE).+?FROM|(CREATE|ALTER|DROP|TRUNCATE)\\s+(TABLE|DATABASE)",
-		'p' => "\\b(and|or)\\b.{1,6}?(=|>|<|\\bin\\b|\\blike\\b)|\\/\\*.+?\\*\\/|<\\s*script\\b|\\bEXEC\\b|UNION.+?SELECT|UPDATE.+?SET|INSERT\\s+INTO.+?VALUES|(SELECT|DELETE).+?FROM|(CREATE|ALTER|DROP|TRUNCATE)\\s+(TABLE|DATABASE)",
-		'c' => "\\b(and|or)\\b.{1,6}?(=|>|<|\\bin\\b|\\blike\\b)|\\/\\*.+?\\*\\/|<\\s*script\\b|\\bEXEC\\b|UNION.+?SELECT|UPDATE.+?SET|INSERT\\s+INTO.+?VALUES|(SELECT|DELETE).+?FROM|(CREATE|ALTER|DROP|TRUNCATE)\\s+(TABLE|DATABASE)",
-	);
-	if (!isset($values)) {
-		return '';
-	}
-	if(is_array($values)) {
-		foreach($values as $key => $val) {
-			$values[addslashes($key)] = strip_gpc($val, $type);
-		}
-	} else {
-		if (preg_match("/".$filter[$type]."/is", $values, $match) == 1) {
-			$values = '';
-		}
-	}
-	return $values;
-}
-
 /**
  * 过滤GET,POST传入的路径中的危险字符
  * @param string $path
@@ -1348,25 +1327,6 @@ function emoji_unicode_encode($string) {
 	preg_match_all('/' . implode('|', $ranges) . '/i', $string, $match);
 	print_r($match);exit;
 }
-
-/**
- * 获取全局变量 $_W 中的值
- * @param string $key
- */
-function getglobal($key) {
-	global $_W;
-	$key = explode('/', $key);
-
-	$v = &$_W;
-	foreach ($key as $k) {
-		if (!isset($v[$k])) {
-			return null;
-		}
-		$v = &$v[$k];
-	}
-	return $v;
-}
-
 
 /**
  *  指定开头的字符串
