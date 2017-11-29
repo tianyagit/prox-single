f680b395eb2df95f6368092c5da615a1ef927ef4
RenChao <donknap@gmail.com>
2017-11-29 17:15:12 +0800

    增加常量中的注释

--- framework/const.inc.php
+++ framework/const.inc.php
@@ -109,10 +109,15 @@ define('CACHE_EXPIRE_MIDDLE', 300);
 define('CACHE_EXPIRE_LONG', 3600);
 define('CACHE_KEY_LENGTH', 100); //缓存键的最大长度
 
+//模块配置信息
 define('CACHE_KEY_MODULE_SETTING', 'module_setting:%s:%s');
+//模块详细信息，包含 CACHE_KEY_MODULE_SETTING
 define('CACHE_KEY_MODULE_INFO', 'module_info:%s');
+//当前公众号及所有者可用的模块
 define('CACHE_KEY_ACCOUNT_MODULES', 'unimodules:%s:%s');
+//模块所有注册菜单
 define('CACHE_KEY_ACCOUNT_MODULES_BINDING', 'unimodules:binding:%s');
+//用户信息
 define('CACHE_KEY_MEMBER_INFO', 'memberinfo:%s');
 define('CACHE_KEY_UNI_GROUP', 'uni_group');
 define('CACHE_KEY_ACCOUNT_SWITCH', 'lastaccount:%s');
