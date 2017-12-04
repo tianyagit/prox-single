8252cbfb3b86b79ba1b250e5373b980d80d0e2b7
malina <1643827412@qq.com>
2017-12-04 19:12:28 +0800

    pc  应用

--- app/source/entry/webapp.ctrl.php
+++ app/source/entry/webapp.ctrl.php
@@ -5,7 +5,7 @@
  */
 
 defined('IN_IA') or exit('Access Denied');
-$entry['module'] = 'we7_diyspecial';
+
 $site = WeUtility::createModuleWebapp($entry['module']);
 if(!is_error($site)) {
 	$method = 'doPage' . ucfirst($entry['do']);
--- framework/model/module.mod.php
+++ framework/model/module.mod.php
@@ -367,6 +367,8 @@ function module_get_all_unistalled($status, $cache = true)  {
 		$account_type = 'wxapp';
 	} elseif (ACCOUNT_TYPE == ACCOUNT_TYPE_OFFCIAL_NORMAL) {
 		$account_type = 'app';
+	} elseif (ACCOUNT_TYPE == ACCOUNT_TYPE_WEBAPP_NORMAL) {
+		$account_type = 'webapp';
 	}
 	if (!is_array($uninstallModules) || empty($uninstallModules['modules'][$status][$account_type]) || intval($uninstallModules['cloud_m_count']) !== intval($cloud_m_count) || is_error($get_cloud_m_count)) {
 		$uninstallModules = cache_build_uninstalled_module();
