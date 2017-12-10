bb54a5a16735f17dd3a8483ba4accc8b36241629
RenChao <donknap@gmail.com>
2017-12-10 18:44:46 +0800

    整理代码

--- web/index.php
+++ web/index.php
@@ -1,26 +1,19 @@
 <?php
 /**
+ * 路由控制器
  * [WeEngine System] Copyright (c) 2013 WE7.CC
  */
 define('IN_SYS', true);
 require '../framework/bootstrap.inc.php';
 require IA_ROOT . '/web/common/bootstrap.sys.inc.php';
 
-load()->web('common');
-load()->web('template');
-load()->func('file');
-load()->model('account');
-load()->model('setting');
-load()->model('user');
-load()->classs('oauth2/oauth2client');
-
 if (!empty($_GPC['state'])) {
-	$callbackParams = OAuth2Client::supportParams($_GPC['state']);
-	if (!empty($callbackParams)) {
+	$login_callback_params = OAuth2Client::supportParams($_GPC['state']);
+	if (!empty($login_callback_params)) {
 		$controller = 'user';
 		$action = 'login';
-		$_GPC['login_type'] = $callbackParams['from'];
-		$_GPC['handle_type'] = $callbackParams['mode'];
+		$_GPC['login_type'] = $login_callback_params['from'];
+		$_GPC['handle_type'] = $login_callback_params['mode'];
 	}
 }
 
