dfa79b9962a07af268fc0460f0356dc610c0c022
Fanyk <javaflex@yeah.net>
2017-12-11 11:11:34 +0800

    方法 属性声明

--- framework/class/app/wechat.class.php
+++ framework/class/app/wechat.class.php
@@ -2,6 +2,13 @@
 /**
  * [WeEngine System] Copyright (c) 2013 WE7.CC
  */
+
+/**
+ * Class Wechat
+ * @property string $bind_domain 绑定的域名
+ *
+ */
 class Wechat extends App {
+
 
 }
--- framework/class/app/wxapp.class.php
+++ framework/class/app/wxapp.class.php
@@ -2,6 +2,16 @@
 /**
  * [WeEngine System] Copyright (c) 2013 WE7.CC
  */
+
+/**
+ * Class Wxapp
+ *
+ * @property $wxapp_domain 小程序设置的域名
+ * @method Wxapp lastVersion() 最新版本
+ * @method Wxapp version($version_id) 指定版本
+ * @property-read array $default_appjson 默认appjson
+ * @property array $custom_appjson 用户配置appjson
+ */
 class Wxapp extends App {
 
 }
