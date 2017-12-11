169b2a2d4d18d93616f07d1dcc944d52fb5c0be1
brjun <brj1001@163.com>
2017-12-11 18:13:51 +0800

    修复访问统计错误

--- framework/model/app.mod.php
+++ framework/model/app.mod.php
@@ -108,12 +108,7 @@ function app_pass_visit_limit($uniacid = 0) {
 		//本月累计大于（设定值+购买量-购买使用量）->返回true
 		$before_num = app_month_visit_till_today($uniacid);
 		$sum_num = intval($limit['founder']) + $order_num - intval($limit['use']);
-		if ($before_num > $sum_num) {
-			$data['limit'] = true;
-			cache_write($cachekey, $data);
-			return true;
-		}
-		if (($before_num + $today_num) > $sum_num) {
+		if ($sum_num <= 0) {
 			$data['limit'] = true;
 			cache_write($cachekey, $data);
 			return true;
