a5f6a85681439e9783f29597bf8e741ae3930901
Fanyk <javaflex@yeah.net>
2017-12-11 20:02:05 +0800

    代码修改

---
+++ framework/table/wechatattachment.table.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * [WeEngine System] Copyright (c) 2013 WE7.CC
+ * User: fanyk
+ * Date: 2017/12/11
+ * Time: 19:50
+ */
+class Wechatattachment extends AttachmentTable {
+
+	protected $table = 'wechat_attachment';
+}
