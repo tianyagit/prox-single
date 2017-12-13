0d5ab1f70101f72f1391bed8eaa77a5802835534
malina <1643827412@qq.com>
2017-12-13 18:55:10 +0800

    代码优化

--- framework/model/mc.mod.php
+++ framework/model/mc.mod.php
@@ -708,7 +708,7 @@ function mc_fans_groups($force_update = false) {
 		$results = iunserializer($results);
 		return $results;
 	}
-	$account_api = WeAccount::create($_W['acid']);
+	$account_api = WeAccount::create($_W['uniacid']);
 	if (!$account_api->isTagSupported()) {
 		return array();
 	}
