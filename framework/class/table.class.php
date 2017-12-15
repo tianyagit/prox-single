ac68673252b1ba16c08418e73e53c15c0ac801bb
Fanyk <javaflex@yeah.net>
2017-12-15 16:04:37 +0800

    table 修改

--- framework/class/table.class.php
+++ framework/class/table.class.php
@@ -12,9 +12,9 @@ defined('IN_IA') or exit('Access Denied');
 abstract class We7Table {
 
 	//表名
-	protected $table_name = '';
+	protected $tableName = '';
 	//主键
-	protected $primary_key = 'id';
+	protected $primaryKey = 'id';
 	protected $query;
 	//数据库属性
 	protected $attributes = array();
@@ -56,10 +56,24 @@ abstract class We7Table {
 	}
 
 
-	protected function fill($attributes) {
+	public function fill($attributes) {
 		$this->attributes = array_merge($this->attributes, $attributes);
+//		$this->query->fill($this->attributes);
+		return $this;
 	}
 
+	/**
+	 *  根据主键获取数据
+	 * @param $id
+	 * @return mixed
+	 */
+	public function findById($id) {
+		$query = $this->query->from($this->tableName)->where($this->primaryKey, $id);
+		if (is_array($id)) {
+			return $query->getall();
+		}
+		return $query->get();
+	}
 	/**
 	 * 追加默认数据
 	 */
@@ -78,6 +92,10 @@ abstract class We7Table {
 	 *  获取字段所有验证规则
 	 */
 	protected function valid($data) {
+		if (count($this->rules) <= 0) {
+			return error(0);
+		}
+
 		$validator = Validator::create($data, $this->rules);
 		$result = $validator->valid();
 		return $result;
@@ -88,33 +106,34 @@ abstract class We7Table {
 	 */
 	public function save(array $attributes = array()) {
 		$this->fill($attributes);
-		$result = $this->valid($attributes);
+		$this->appendDefauls();
+		$result = $this->valid($this->attributes);
 		if (is_error($result)) {
 			return $result;
 		}
-		//追加默认值
-		$this->appendDefauls();
-		return pdo_insert($this->table_name, $this->attributes);
+//
+		$result = pdo_insert($this->tableName, $this->attributes);
+		if ($result) {
+			$this->attributes[$this->primaryKey] = pdo_insertid();
+		}
+		return $result;
+//		return $this->query->save();
 	}
 
-	/**
-	 * 更新数据
-	 * @param array $attributes
-	 */
-	public function update(array $data = array(), $params = array()) {
-		$result = $this->valid($data);
-		if (is_error($result)) {
-			return $result;
-		}
-		return pdo_update($this->table_name, $data, $params);
+	public function update() {
+		return $this->query->save();
 	}
 
 	/** 删除数据
 	 * @param $value
 	 * @return mixed
 	 */
-	public function delete(array $params) {
-		return pdo_delete($this->table_name, $params);
+	public function delete() {
+		return $this->query->delete();
+	}
+
+	private function doWhere($field, $params) {
+		return $this->query->where(lcfirst($field), $params);
 	}
 
 
@@ -137,6 +156,15 @@ abstract class We7Table {
 		if(method_exists($this, $search_method)) {
 			return call_user_func_array(array($this, $search_method), $params);
 		}
+
+		if(starts_with($method, 'updateWith')) {
+			return $this->doWhere(str_replace('updateWith', '', $method), $params);
+		}
+
+		if(starts_with($method, 'deleteWith')) {
+			return $this->doWhere(str_replace('deleteWith', '', $method), $params);
+		}
+
 		return call_user_func_array(array($this->query, $method), $params);
 	}
 }
