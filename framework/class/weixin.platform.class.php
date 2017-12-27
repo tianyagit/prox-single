a805a41ca89fda6b4f51ea21df4c35de96367f5f
brjun <brj1001@163.com>
2017-12-27 16:51:12 +0800

    恢复代码

--- framework/class/weixin.platform.class.php
+++ framework/class/weixin.platform.class.php
@@ -201,7 +201,7 @@ class WeiXinPlatform extends WeiXinAccount {
 		if (is_error($response)) {
 			return $response;
 		}
-		cache_write('account:oauth:refreshtoken:'.$this->account['key'], $response['refresh_token']);
+		cache_write('account:oauth:refreshtoken:'.$this->account['account_appid'], $response['refresh_token']);
 		return $response;
 	}
 
