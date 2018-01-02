8a88e6b362720587ed41cce378e1e2e5e48a72df
malina <1643827412@qq.com>
2018-01-02 16:41:21 +0800

    工单  6个小时  请求一次

--- framework/model/message.mod.php
+++ framework/model/message.mod.php
@@ -152,6 +152,11 @@ function message_notice_worker() {
 	if (!empty($message_record)) {
 		$time = $message_record['create_time'];
 	}
+
+	if (!empty($time) && TIMESTAMP - $time < 60 * 60 * 6) {
+		return true;
+	}
+
 	$api_url = $api->get('system', 'workorder', array('do' => 'notload', 'time' => $time), 'json', false);
 	if (is_error($api_url)) {
 		return true;
