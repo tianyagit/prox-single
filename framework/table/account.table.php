862d687ac394c2ac58cacc99fc959ad94c372c59
malina <1643827412@qq.com>
2018-01-26 11:19:10 +0800

    APP

--- framework/table/account.table.php
+++ framework/table/account.table.php
@@ -127,6 +127,18 @@ class AccountTable extends We7Table {
 				->getall('acid');
 	}
 
+	public function accountPhoneappInfo($uniacids, $uid) {
+		return $this->query->from('uni_account', 'a')
+				->leftjoin('account_phoneapp', 'w')
+				->on(array('w.uniacid' => 'a.uniacid'))
+				->leftjoin('uni_account_users', 'au')
+				->on(array('a.uniacid' => 'au.uniacid'))
+				->where(array('a.uniacid' => $uniacids))
+				->where(array('au.uid' => $uid))
+				->orderby('a.uniacid', 'asc')
+				->getall('acid');
+	}
+
 	public function searchWithKeyword($title) {
 		$this->query->where('a.name LIKE', "%{$title}%");
 		return $this;
