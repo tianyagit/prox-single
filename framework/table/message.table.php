ce87bbee03b1bea50681fd743c7a34f9491755b0
malina <1643827412@qq.com>
2018-01-31 17:08:06 +0800

    message.model   table

--- framework/table/message.table.php
+++ framework/table/message.table.php
@@ -8,6 +8,9 @@ defined('IN_IA') or exit('Access Denied');
 
 class MessageTable extends We7Table {
 
+	protected $tableName = 'message_notice_log';
+	protected $field = array('id', 'message', 'is_read', 'uid', 'sign', 'type', 'status', 'create_time', 'end_time');
+
 	public function messageList($type = '') {
 		global $_W;
 
@@ -19,23 +22,14 @@ class MessageTable extends We7Table {
 			$this->query->where('type !=', array(MESSAGE_USER_EXPIRE_TYPE));
 		}
 
-		return $this->query->from('message_notice_log')->orderby('id', 'DESC')->getall();
+		return $this->query->from($this->tableName)->orderby('id', 'DESC')->getall();
 
 	}
 
 	public function messageRecord() {
-		return $this->query->from('message_notice_log')->orderby('id', 'DESC')->get();
+		return $this->query->from($this->tableName)->orderby('id', 'DESC')->get();
 	}
 
-	public function searchWithType($type) {
-		$this->query->where('type', $type);
-		return $this;
-	}
-
-	public function searchWithIsRead($is_read) {
-		$this->query->where('is_read', $is_read);
-		return $this;
-	}
 
 	public function messageNoReadCount() {
 		global $_W;
@@ -45,7 +39,11 @@ class MessageTable extends We7Table {
 		if (user_is_founder($_W['uid']) && !user_is_vice_founder()) {
 			$this->query->where('type !=', array(MESSAGE_USER_EXPIRE_TYPE));
 		}
-		$list =  $this->query->from('message_notice_log')->orderby('id', 'DESC')->getall();
+		$list =  $this->query->from($this->tableName)->orderby('id', 'DESC')->getall();
 		return count($list);
+	}
+
+	public function messageExists($message) {
+		return $this->query->from($this->tableName)->where($message)->get();
 	}
 }
