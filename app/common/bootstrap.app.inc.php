9fe9acb6a2215106abf1ae7f4702029a0a852acf
RenChao <donknap@gmail.com>
2018-03-11 15:37:05 +0800

    整 理代码

--- app/common/bootstrap.app.inc.php
+++ app/common/bootstrap.app.inc.php
@@ -29,14 +29,14 @@ $isdel_account = pdo_get('account', array('isdeleted' => 1, 'acid' => $_W['acid'
 if (!empty($isdel_account)) {
 	exit('指定公众号已被删除');
 }
-/* xstart */
+/* sxstart */
 if (IMS_FAMILY == 's' || IMS_FAMILY == 'x') {
 	if (!empty($_W['account']['setting']['bind_domain']) && !empty($_W['account']['setting']['bind_domain']['domain']) && strpos($_W['siteroot'], $_W['account']['setting']['bind_domain']['domain']) === false) {
 		header('Location:' . $_W['account']['setting']['bind_domain']['domain']. $_SERVER['REQUEST_URI']);
 		exit;
 	}
 }
-/* xend */
+/* sxend */
 $_W['session_id'] = '';
 if (isset($_GPC['state']) && !empty($_GPC['state']) && strexists($_GPC['state'], 'we7sid-')) {
 	$pieces = explode('-', $_GPC['state']);
