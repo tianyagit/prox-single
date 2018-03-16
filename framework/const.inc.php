a636b1cf950d2c007855ead1f17c6807151d485d
lgl <13653668463>
2018-03-16 18:44:41 +0800

    用户权限组配置

--- framework/const.inc.php
+++ framework/const.inc.php
@@ -223,6 +223,7 @@ define('STORE_TYPE_PACKAGE', 5);
 define('STORE_TYPE_API', 6);
 define('STORE_TYPE_ACCOUNT_RENEW', 7);
 define('STORE_TYPE_WXAPP_RENEW', 8);
+define('STORE_TYPE_USER_PACKAGE', 9);
 //订单状态
 define('STORE_ORDER_PLACE', 1);
 define('STORE_ORDER_DELETE', 2);
