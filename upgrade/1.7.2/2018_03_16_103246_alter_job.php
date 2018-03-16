baf02e360af9ba23dd398616a26b6f0d7228c9f9
fykyx521 <javaflex@yeah.net>
2018-03-16 11:40:20 +0800

    删除任务自动执行

---
+++ upgrade/1.7.2/2018_03_16_103246_alter_job.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace We7\V172;
+
+defined('IN_IA') or exit('Access Denied');
+/**
+ * [WeEngine System] Copyright (c) 2013 WE7.CC
+ * Time: 1521167566
+ * @version 1.7.2
+ */
+
+class AlterJob {
+
+	/**
+	 *  执行更新
+	 */
+	public function up() {
+		if (! pdo_fieldexists('job', 'uid')) {
+			$tableName = tablename('job');
+			$sql = <<<EOT
+					ALTER TABLE $tableName ADD COLUMN uid int(11) DEFAULT 0;
+					ALTER TABLE $tableName ADD COLUMN isdeleted TINYINT(1) DEFAULT 0;
+EOT;
+			pdo_run($sql);
+		}
+
+		if (! pdo_fieldexists('job', 'isdeleted')) {
+			$tableName = tablename('job');
+			$sql = <<<EOT
+					ALTER TABLE $tableName ADD COLUMN isdeleted TINYINT(1) DEFAULT 0;
+EOT;
+			pdo_run($sql);
+		}
+	}
+	
+	/**
+	 *  回滚更新
+	 */
+	public function down() {
+		
+
+	}
+}
+		
