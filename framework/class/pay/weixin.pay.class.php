d7b56f343d21be0fa0fb931d1625b9b708167601
brjun <brj1001@163.com>
2018-03-27 15:19:13 +0800

    退款临时解决方案

--- framework/class/pay/weixin.pay.class.php
+++ framework/class/pay/weixin.pay.class.php
@@ -25,7 +25,7 @@ class WeiXinPay extends pay{
 			$this->wxpay = array(
 				'appid' => $_W['account']['key'],
 				'mch_id' => $wxpay['mchid'],
-				'key' => $wxpay['apikey'],
+				'key' => !empty($wxpay['apikey']) ? $wxpay['apikey'] : $wxpay['signkey'],  //暂时解决方案，todo
 				'notify_url' => $_W['siteroot'] . 'payment/wechat/notify.php',
 			);
 		}
@@ -305,7 +305,7 @@ class WeiXinPay extends pay{
 			'signType' => 'MD5',
 		);
 		$jspai['paySign'] = $this->bulidSign($jspai);
-		
+
 		$jspai = <<<EOF
 		<script type="text/javascript">
 			document.addEventListener('WeixinJSBridgeReady', function onBridgeReady() {
