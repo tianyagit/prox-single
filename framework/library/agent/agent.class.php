1d88e5a9c323e4fc4385b56c678dcab0fffe9d35
lgl <13653668463>
2018-04-03 11:55:01 +0800

    isMobile()方法-增加电脑端微信登录判断

--- framework/library/agent/agent.class.php
+++ framework/library/agent/agent.class.php
@@ -165,7 +165,7 @@ class Agent
 			$clientkeywords = array('nokia', 'sony', 'ericsson', 'mot', 'samsung', 'htc', 'sgh', 'lg', 'sharp',
 				'sie-', 'philips', 'panasonic', 'alcatel', 'lenovo', 'iphone', 'ipod', 'blackberry', 'meizu', 
 				'android', 'netfront', 'symbian', 'ucweb', 'windowsce', 'palm', 'operamini', 'operamobi', 'openwave', 
-				'nexusone', 'cldc', 'midp', 'wap', 'mobile');
+				'nexusone', 'cldc', 'midp', 'wap', 'mobile', 'WindowsWechat');
 			// 从HTTP_USER_AGENT中查找手机浏览器的关键字
 			if (preg_match("/(" . implode('|', $clientkeywords) . ")/i", strtolower($_SERVER['HTTP_USER_AGENT']))) {
 				return true;
