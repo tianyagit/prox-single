5ee01b041b2dad8d5ccd90dcda0d8bae6b7f4ad5
lgl <13653668463>
2018-04-17 16:10:33 +0800

    头部菜单-应用详情页链接修改

--- web/source/module/display.ctrl.php
+++ web/source/module/display.ctrl.php
@@ -87,7 +87,7 @@ if ($do == 'switch') {
 	if (empty($uniacid) && !empty($version_id)) {
 		uni_account_save_switch($version_info['uniacid'], WXAPP_TYPE_SIGN);
 		wxapp_update_last_use_version($version_info['uniacid'], $version_id);
-		itoast('', url('account/display/switch', array('module' => $module_name, 'version_id' => $version_id, 'type' => ACCOUNT_TYPE_APP_NORMAL)), 'success');
+		itoast('', url('account/display/switch', array('uniacid' => $uniacid, 'module' => $module_name, 'version_id' => $version_id, 'type' => ACCOUNT_TYPE_APP_NORMAL)), 'success');
 	}
 	if (!empty($uniacid)) {
 		if (empty($version_id)) {
