c855ba1cd51fef47682276b64db4c9ef750ad3a8
brjun <brj1001@163.com>
2018-04-23 15:52:57 +0800

    整理代码

--- framework/version.inc.php
+++ framework/version.inc.php
@@ -6,5 +6,5 @@
 defined('IN_IA') or exit('Access Denied');
 
 define('IMS_FAMILY', 'x');
-define('IMS_VERSION', '1.7.2');
+define('IMS_VERSION', '1.7.4');
 define('IMS_RELEASE_DATE', '20140807141111');
