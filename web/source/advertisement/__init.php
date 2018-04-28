13ec3a9af70c0580030259ab1eed20c26e601092
lgl <13653668463>
2018-04-28 11:46:27 +0800

    代码整理

--- web/source/advertisement/__init.php
+++ web/source/advertisement/__init.php
@@ -5,9 +5,8 @@
  * Date: 2017/1/6
  * Time: 16:28
  */
-itoast('访问链接已失效！', referer(), 'error');
-exit();
 header('Location: ' . url('account/display'));
+exit();
 defined('IN_IA') or exit('Access Denied');
 define('FRAME', 'advertisement');
 if ($do == 'display') {
