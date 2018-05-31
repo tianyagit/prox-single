9254827e13b1eea96740cf6fecbcdd69ca203a96
brjun <brj1001@163.com>
2018-05-31 11:23:38 +0800

    添加用户时增加填写的副创始人姓名是否为副创始人

--- web/source/user/create.ctrl.php
+++ web/source/user/create.ctrl.php
@@ -10,6 +10,14 @@ load()->model('user');
 $_W['page']['title'] = '添加用户 - 用户管理';
 
 if (checksubmit()) {
+	$vice_founder_name = safe_gpc_string($_GPC['vice_founder_name']);
+	$is_founder = user_single(array('username' => $vice_founder_name));
+	if (empty($is_founder)) {
+		itoast('副创始人不存在！');
+	}
+	if (!user_is_vice_founder($is_founder['uid'])) {
+		itoast('请勿添加非副创始人姓名！');
+	}
 	$user_founder = array(
 		'username' => safe_gpc_string($_GPC['username']),
 		'password' => $_GPC['password'],
@@ -18,7 +26,7 @@ if (checksubmit()) {
 		'groupid' => intval($_GPC['groupid']),
 		'starttime' => TIMESTAMP,
 		'endtime' => intval($_GPC['timelimit']),
-		'vice_founder_name' => safe_gpc_string($_GPC['vice_founder_name'])
+		'vice_founder_name' => $vice_founder_name
 	);
 
 	$user_add = user_info_save($user_founder);
