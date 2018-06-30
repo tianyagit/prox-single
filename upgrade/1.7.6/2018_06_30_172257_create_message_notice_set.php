b21553145f7fbcb4265098025521dbcf9298d0e7
brjun <brj1001@163.com>
2018-06-30 17:23:12 +0800

    升级脚本

---
+++ upgrade/1.7.6/2018_06_30_172257_create_message_notice_set.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace We7\V176;
+
+defined('IN_IA') or exit('Access Denied');
+/**
+ * [WeEngine System] Copyright (c) 2013 WE7.CC
+ * Time: 1530350577
+ * @version 1.7.6
+ */
+
+class CreateMessageNoticeSet {
+
+	/**
+	 *  执行更新
+	 */
+	public function up() {
+
+	}
+	
+	/**
+	 *  回滚更新
+	 */
+	public function down() {
+		
+
+	}
+}
+		
