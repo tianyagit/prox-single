822fd1b08dcdee6bc231605715f826ec2a79f38a
brjun <brj1001@163.com>
2018-07-08 22:25:30 +0800

    优化用户体验

--- web/source/webapp/rewrite.ctrl.php
+++ web/source/webapp/rewrite.ctrl.php
@@ -15,5 +15,8 @@ if ($do == 'display') {
 	if (!empty($_W['account']['setting']['default_module'])) {
 		$eid = table('modules_bindings')->where('module', $_W['account']['setting']['default_module'])->getcolumn('eid');
 	}
+	if (!empty($_W['account']['setting']['bind_domain']) && !empty($eid)) {
+		$url = current($_W['account']['setting']['bind_domain']) . '/' . $_W['uniacid'] . '-' . $eid .'.html';
+	}
 	template('webapp/rewrite');
 }
