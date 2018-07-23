dd042f582ad7578c0566aec0f97354aed1359969
lgl <13653668463@163.com>
2018-07-23 09:25:49 +0800

    代码整理-粉丝标签tagid 修改类型

--- upgrade/1.7.8/2018_07_23_091408_alter_mc_fans_tag_mapping_tagid_varchar.php
+++ upgrade/1.7.8/2018_07_23_091408_alter_mc_fans_tag_mapping_tagid_varchar.php
@@ -5,18 +5,18 @@ namespace We7\V178;
 defined('IN_IA') or exit('Access Denied');
 /**
  * [WeEngine System] Copyright (c) 2013 WE7.CC
- * Time: 1532058735
+ * Time: 1532308448
  * @version 1.7.8
  */
 
-class AlterMcFansTagMappingTagid {
+class AlterMcFansTagMappingTagidVarchar {
 
 	/**
 	 *  执行更新
 	 */
 	public function up() {
 		if (pdo_fieldexists('mc_fans_tag_mapping','tagid')) {
-			pdo_query("ALTER TABLE " . tablename('mc_fans_tag_mapping') . " MODIFY COLUMN  `tagid` BIGINT NOT NULL ;");
+			pdo_query("ALTER TABLE " . tablename('mc_fans_tag_mapping') . " MODIFY COLUMN  `tagid` VARCHAR(20) NOT NULL ;");
 		}
 	}
 	
