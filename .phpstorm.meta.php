2ac7ca667c6037640ec65245f6dd90f1e5be0414
brjun <brj1001@163.com>
2018-08-06 17:24:26 +0800

    完善信息

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -12,6 +12,11 @@ namespace PHPSTORM_META {
 	    'modules_rank' => \We7\Table\Modules\Rank::class,
 		'uni_account_modules' => \We7\Table\Uni\AccountModules::class,
 		'system_stat_visit' => \We7\Table\System\StatVisit::class,
-		'article_comment' => \We7\Table\Article\comment::class,
+		'article_comment' => \We7\Table\Article\Comment::class,
+		'account_xzapp' => \We7\Table\Account\Xzapp::class,
+		'account_aliapp' => \We7\Table\Account\Aliapp::class,
+		'account_wxapp' => \We7\Table\Account\Wxapp::class,
+		'core_profile_fields' => \We7\Table\Core\ProfileFields::class,
+		'wxapp_versions' => \We7\Table\Wxapp\Versions::class,
     ]));
 }
